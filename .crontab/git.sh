366a37b7b04c3da221e7c66daa73dccfa86e6f32
hanice <sunhanice@gmail.com>
2014-07-17 10:09:05 +0200

    hanice

--- .crontab/git.sh
+++ .crontab/git.sh
@@ -1,5 +1,7 @@
+#crontab -e
+# */30 * * * * bash /g/steinmetz/hsun/.crontab/git.sh
 cd /g/steinmetz/hsun/
 git add *.py
 git add *.sh
-git commit -m 'hanice'
+git commit -a -m 'hanice'
 git push origin master
