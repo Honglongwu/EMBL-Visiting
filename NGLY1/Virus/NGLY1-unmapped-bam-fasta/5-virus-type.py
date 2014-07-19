7736f7a892a759c911b374a39e61c917d0e352df
hanice <sunhanice@gmail.com>
2014-07-19 15:30:03 +0200

    hanice

--- NGLY1/Virus/NGLY1-unmapped-bam-fasta/5-virus-type.py
+++ NGLY1/Virus/NGLY1-unmapped-bam-fasta/5-virus-type.py
@@ -1,14 +1,20 @@
 inFile = open('NGLY1-Viruses-mapped2')
 ouFile = open('NGLY1-Viruses-mapped2-virus-type', 'w')
 D = {}
+D2 = {}
 while True:
     line1 = inFile.readline().strip()
     line2 = inFile.readline().strip()
     if line1:
         fields = line1.split('\t')
         virus= fields[1]
-        D.setdefault(virus, 0)
-        D[virus] += 1
+        virus_pos = virus+':'+fields[8]+':'+fields[9]
+        if virus_pos in D2:
+            pass
+        else:
+            D.setdefault(virus, 0)
+            D[virus] += 1
+            D2[virus_pos] = 1
     else:
         break
 inFile.close()
--- NGLY1/Virus/NGLY1-unmapped-bam-fasta/test.sh
+++
@@ -1,2 +0,0 @@
-cd /g/steinmetz/hsun/NGLY1/Virus/NGLY1-unmapped-bam-fasta
-qsub ngly1_ngly1_FCP1DMSOrep2_lane8.blat.sh
