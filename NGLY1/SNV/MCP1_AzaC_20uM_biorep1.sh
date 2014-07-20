d1852b58cd5dc859924bc2ad867eeb3b683cbb96
hanice <sunhanice@gmail.com>
2014-07-20 13:30:02 +0200

    hanice

--- NGLY1/SNV/MCP1_AzaC_20uM_biorep1.sh
+++ NGLY1/SNV/MCP1_AzaC_20uM_biorep1.sh
@@ -1,3 +1,3 @@
-cd /g/steinmetz/hsun/NGLY1/SNV/alignment_filtered
+cd /g/steinmetz/hsun/NGLY1/SNV/
 #samtools mpileup -uDf /g/steinmetz/hsun/NGLY1/SNV/Homo_sapiens.GRCh37.68.dna.chromosomes.withIVTs.fa MCP1_AzaC_20uM_biorep1.bam | bcftools view -bvcg - > MCP1_AzaC_20uM_biorep1.raw.vcf 
-bcftools view MCP1_AzaC_20uM_biorep1.raw.vcf |/g/steinmetz/hsun/MySoft/samtools/samtools-0.1.19/bcftools/vcfutils.pl varFilter - > MCP1_AzaC_20uM_biorep1.flt.vcf
+bcftools view MCP1_AzaC_20uM_biorep1.raw.vcf |vcfutils.pl varFilter  > MCP1_AzaC_20uM_biorep1.flt.vcf
