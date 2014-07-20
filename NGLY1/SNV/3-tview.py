422ab63676acf25ecb040b29e2bf56fb82b7d1f1
hanice <sunhanice@gmail.com>
2014-07-20 14:30:03 +0200

    hanice

--- NGLY1/SNV/3-tview.py
+++ NGLY1/SNV/3-tview.py
@@ -1,11 +1,12 @@
 inFile = open('NGLY1-SNV')
 ouFile = open('NGLY1-SNV.sh', 'w')
+I = 65
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    pos = fields[3] + ':' + fields[4]
+    pos = fields[3] + ':' + str(int(fields[4])-I)
     bam = fields[0].split('flt.annovar.exonic_variant_function')[0] + 'bam'
-    ouFile.write('samtools tview %s $hg19 -p %s\n'%(bam, pos))
+    ouFile.write('samtools tview %s /g/steinmetz/hsun/NGLY1/Genome/Homo_sapiens.GRCh37.68.dna.chromosomes.withIVTs.fa -p %s\n'%(bam, pos))
 
 inFile.close()
 ouFile.close()
