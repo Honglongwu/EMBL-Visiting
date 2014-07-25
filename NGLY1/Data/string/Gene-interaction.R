89a5453b9ef894e4feeb3b86ac0c71f2a1498f2f
hanice <sunhanice@gmail.com>
2014-07-25 11:00:04 +0200

    hanice

---
+++ NGLY1/Data/string/Gene-interaction.R
@@ -0,0 +1,24 @@
+library(GenomicAlignments)
+
+Score = 700
+homeFolder = '/g/steinmetz/wmueller/NGLY1'
+STRING = '/g/steinmetz/hsun/NGLY1/Data/string/9606.protein.links.v9.1.txt'
+
+
+interaction = function(genename)
+{
+load(file.path(homeFolder, 'gtf.rda'))
+proteinid = unique(mcols(gtf)[mcols(gtf)$gene_name == genename,]$protein_id)
+proteinid = proteinid[!is.na(proteinid)]
+proteinid = paste0('9606.', proteinid)
+
+string = read.table(STRING, header=T)
+s = string[string$protein1 %in% proteinid & string$combined_score >= Score,]
+partner = unique(mcols(gtf)[paste0('9606.',mcols(gtf)$protein_id) %in% s$protein2,]$gene_name)
+write.table(partner,paste0(genename,'-partners'),quote=F,row.names=F,col.names=F)
+}
+
+interaction('NGLY1')
+interaction('SRSF2')
+interaction('MYC')
+interaction('TP53')
