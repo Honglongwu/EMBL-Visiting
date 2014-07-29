cbbe3ed41e56a99ad4d2d022ad5c929316e76601
hanice <sunhanice@gmail.com>
2014-07-29 11:30:03 +0200

    hanice

--- Code-SVN/ngly1/DE/de_exon_NGLY1.R
+++ Code-SVN/ngly1/DE/de_exon_NGLY1.R
@@ -12,8 +12,8 @@ load(file.path(folder, "sampleAnnot-CP4.rda"))
 ## first look at gene counts
 mat = assay(exonCounts)
 ## use all samples
-wh = rep(TRUE, ncol(mat))
-wh = sampleAnnot$treatment!='DMSO' & sampleAnnot$individual!='19'
+#wh = rep(TRUE, ncol(mat))
+wh = sampleAnnot$treatment=='DMSO' & sampleAnnot$individual!='19'
 #wh = sampleAnnot$individual == "19"
 mat = mat[, wh]
 sampleAnnot = droplevels(sampleAnnot[wh, ])
@@ -23,7 +23,7 @@ mygenes = exonicParts[sel]
 mat = mat[sel,]
 
 dxd = DEXSeqDataSet(mat, sampleAnnot,
-    design= ~ individual + treatment+exon+treatment:exon,
+    design= ~ individual + exon,
     featureID=as.character(mcols(mygenes)$exonic_part), 
     groupID=as.character(mcols(mygenes)$gene_id),
     featureRanges=mygenes
@@ -34,20 +34,21 @@ ncpu=20
 dxd = DEXSeq::estimateSizeFactors( dxd )
 dxd = DEXSeq::estimateDispersions( dxd , BPPARAM=MulticoreParam(workers=ncpu))
 dxd = testForDEU( dxd, reducedModel=~ individual + treatment+exon,   BPPARAM=MulticoreParam(workers=ncpu))
-dxd = estimateExonFoldChanges( dxd, fitExpToVar="treatment", BPPARAM=MulticoreParam(workers=ncpu))
+dxd = estimateExonFoldChanges( dxd, BPPARAM=MulticoreParam(workers=ncpu))
 dxr1 = DEXSeqResults( dxd )
 
-save(dxd, dxr1, file=file.path(outfolder, "DE_all.rda"))
+save(dxd, dxr1, file=file.path(outfolder, "DE_all_NGLY1_DMSO.rda"))
 
-plotFolder=file.path(outfolder, "plot")
-if (!file.exists(plotFolder))  dir.create(plotFolder)
 
-pdf(file.path(plotFolder, "DE_all_SRSF.pdf"), width=12, height=8)
-for(thisExon in paste0("SRSF",1:12)){
-    plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE, 
-        fitExpToVar="treatment",norCounts=TRUE, splicing=TRUE)
-}
-dev.off()
+#plotFolder=file.path(outfolder, "plot")
+#if (!file.exists(plotFolder))  dir.create(plotFolder)
+#
+#pdf(file.path(plotFolder, "DE_all_SRSF.pdf"), width=12, height=8)
+#for(thisExon in paste0("SRSF",1:12)){
+#    plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE, 
+#        fitExpToVar="treatment",norCounts=TRUE, splicing=TRUE)
+#}
+#dev.off()
 
 
 
