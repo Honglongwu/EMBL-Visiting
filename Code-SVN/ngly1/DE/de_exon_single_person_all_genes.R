690a86bdd1a19bbc38e2a59dc1a63eadb6d9a239
hanice <sunhanice@gmail.com>
2014-07-29 10:00:03 +0200

    hanice

--- Code-SVN/ngly1/DE/de_exon_single_person_all_genes.R
+++ Code-SVN/ngly1/DE/de_exon_single_person_all_genes.R
@@ -14,7 +14,7 @@ sigplot = function(sample,gene){
     plotFolder = file.path(folder, "plot")
     if (!file.exists(plotFolder))  dir.create(plotFolder)
 
-    pdf(file.path(plotFolder, paste0(sample,"exon_sig.pdf")), width=12, height=8)
+    pdf(file.path(plotFolder, paste0(unlist(strsplit(sample, "[.]"))[1],"_exon_sig.pdf")), width=12, height=8)
     for(thisExon in gene){
         plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE, 
             fitExpToVar="treatment",norCounts=TRUE, splicing=TRUE)
@@ -23,70 +23,11 @@ sigplot = function(sample,gene){
 }
 siggenes = lapply(c('DE_CP1.rda', 'DE_CP2.rda', 'DE_CP3.rda', 
                'DE_CP4.rda', 'DE_MCP1.rda', 'DE_FCP1.rda'), sigget)
-names(sig) = c('CP1', 'CP2', 'CP3', 'CP4', 'MCP1', 'FCP1')
-six_samples = Reduce(intersect, siggenes[1:5])
-four_patients = Reduce(intersect, siggenes[1:4])
+names(siggenes) = c('CP1', 'CP2', 'CP3', 'CP4', 'MCP1', 'FCP1')
+lapply(siggenes,length)
+six_samples = Reduce(intersect, siggenes[1:6])
+four_patients = Reduce(intersect, siggenes[1:7])
 print(six_samples)
 print(four_patients)
 lapply(c('DE_CP1.rda', 'DE_CP2.rda', 'DE_CP3.rda', 
          'DE_CP4.rda', 'DE_MCP1.rda', 'DE_FCP1.rda'), sigplot,gene=four_patients)
-
-
-
-
-
-
-
-
-library(DEXSeq)
-ncpu=35
-
-folder = "/g/steinmetz/wmueller/NGLY1/"
-load(file.path(folder, "exon-CP4/exonCounts-CP4.rda"))
-load(file.path(folder, "sampleAnnot-CP4.rda"))
-outfolder = file.path(folder, "exon-CP4")
-if (!file.exists(outfolder))  dir.create(outfolder)
-
-## first look at gene counts
-mat = assay(exonCounts)
-
-## exclude sample 19 for the time being
-for(thisIndividual in levels(sampleAnnot$individual)){
-    wh = sampleAnnot$individual == thisIndividual 
-    
-    submat = mat[, wh]
-    subSampleAnnot = droplevels(sampleAnnot[wh, ])
-    
-    selGene = unique(as.character(exonicParts$gene_id))
-    sel = as.logical(mcols(exonicParts)$gene_id %in% selGene)
-    mygenes = exonicParts[sel]
-    submat = submat[sel,]
-    
-    dxd = DEXSeqDataSet(submat, subSampleAnnot,
-        design= ~ treatment + exon+treatment:exon,
-        featureID=as.character(mcols(mygenes)$exonic_part), 
-        groupID=as.character(mcols(mygenes)$gene_id),
-        featureRanges=mygenes
-#transcripts=as.character(mcols(mygenes)$tx_name)  
-    )
-    
-    dxd = DEXSeq::estimateSizeFactors( dxd )
-    dxd = DEXSeq::estimateDispersions( dxd , BPPARAM=MulticoreParam(workers=ncpu))
-    dxd = testForDEU( dxd, reducedModel=~ treatment + exon,   BPPARAM=MulticoreParam(workers=ncpu))
-    dxd = estimateExonFoldChanges( dxd, fitExpToVar="treatment", BPPARAM=MulticoreParam(workers=ncpu))
-    dxr1 = DEXSeqResults( dxd )
-    save(dxd, dxr1, file=file.path(outfolder, paste0("DE_", thisIndividual, ".rda")))
-    
-#plotDispEsts( dxd )
-    
-    plotFolder = file.path(outfolder, "plot")
-    if (!file.exists(plotFolder))  dir.create(plotFolder)
-    
-    pdf(file.path(plotFolder, paste0(thisIndividual,"_ngly1_srsf2.pdf")), width=12, height=8)
-    for(thisExon in c("NGLY1", "SRSF2")){
-        plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE, 
-            fitExpToVar="treatment",norCounts=TRUE, splicing=TRUE)
-    }
-    dev.off()    
-}
-
