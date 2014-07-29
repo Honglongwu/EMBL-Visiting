884f68db081d767c4e8cf42845705d592208eade
hanice <sunhanice@gmail.com>
2014-07-29 15:30:01 +0200

    hanice

--- Code-SVN/ngly1/DE/DESeq_ngly1_intersection.R
+++ Code-SVN/ngly1/DE/DESeq_ngly1_intersection.R
@@ -9,11 +9,18 @@ cp1mcp1fcp1siggene=rownames(cp1mcp1fcp1[cp1mcp1fcp1$padj<0.01 & !is.na(cp1mcp1fc
 cp1cp4mcp1siggene=rownames(cp1cp4mcp1[cp1cp4mcp1$padj<0.01 & !is.na(cp1cp4mcp1$padj),])
 cp2cp3fcp1siggene=rownames(cp2cp3fcp1[cp2cp3fcp1$padj<0.01 & !is.na(cp2cp3fcp1$padj),])
 three_sets=intersect(intersect(cp1mcp1fcp1siggene,cp1cp4mcp1siggene),cp2cp3fcp1siggene)
+two_sets=intersect(cp1cp4mcp1siggene,cp2cp3fcp1siggene)
 load('/g/steinmetz/wmueller/NGLY1/gtf.rda')
 genes = ids[ids$gene_id %in% three_sets,]
 write.table(genes,'DE_ngly1_threesets_genes.txt',quote=F,row.names=F, col.names=F)
 write.table(genes[1],'DE_ngly1_threesets_genes_id.txt',quote=F,row.names=F, col.names=F)
 write.table(genes[2],'DE_ngly1_threesets_genes_symbol.txt',quote=F,row.names=F, col.names=F)
+
+genes = ids[ids$gene_id %in% two_sets,]
+write.table(genes,'DE_ngly1_twosets_genes.txt',quote=F,row.names=F, col.names=F)
+write.table(genes[1],'DE_ngly1_twosets_genes_id.txt',quote=F,row.names=F, col.names=F)
+write.table(genes[2],'DE_ngly1_twosets_genes_symbol.txt',quote=F,row.names=F, col.names=F)
+
 
 
 load(file.path(folder,'resNGLY1-ALL.rda'))
