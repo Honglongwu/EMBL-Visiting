104c124637e20d875b0ddfee8cb5a3c2c9564ba6
hanice <sunhanice@gmail.com>
2014-07-29 16:00:04 +0200

    hanice

---
+++ Code-SVN/ngly1/DE/de_exon_NGLY1-plot.R
@@ -0,0 +1,24 @@
+# test difference at exon level for all data
+# Author: czhu
+###############################################################################
+library(DEXSeq)
+
+folder = "/g/steinmetz/wmueller/NGLY1"
+outfolder = file.path(folder, "exon-CP4")
+
+load(file.path(outfolder, "DE_all_NGLY1_DMSO.rda"))
+
+sigplot=function(gene){
+plotFolder=file.path(outfolder, "plot")
+if (!file.exists(plotFolder))  dir.create(plotFolder)
+
+pdf(file.path(plotFolder, paste0("DE_all_NGLY1_DMSO",paste(gene[1:3],collapse='_'),".pdf")), width=12, height=8)
+for(thisExon in gene){
+    plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE, 
+        fitExpToVar="individual",norCounts=TRUE, splicing=TRUE)
+}
+dev.off()
+}
+
+sigplot(c('MYC','TP53','NGLY1'))
+sigplot(c('C5orf45','EIF3I','ENOSF1','LRRFIP1','RGS3','SQSTM1'))
--- Code-SVN/ngly1/DE/de_exon_NGLY1.R
+++ Code-SVN/ngly1/DE/de_exon_NGLY1.R
@@ -34,7 +34,7 @@ ncpu=10
 dxd = DEXSeq::estimateSizeFactors( dxd )
 dxd = DEXSeq::estimateDispersions( dxd , BPPARAM=MulticoreParam(workers=ncpu))
 dxd = testForDEU( dxd, reducedModel=~ individual + gender + exon,BPPARAM= MulticoreParam(workers=ncpu))
-#dxd = estimateExonFoldChanges( dxd, BPPARAM=MulticoreParam(workers=ncpu))
+dxd = estimateExonFoldChanges( dxd,fitExpToVar="individual", BPPARAM=MulticoreParam(workers=ncpu))
 dxr1 = DEXSeqResults( dxd )
 save(dxd, dxr1, file=file.path(outfolder, "DE_all_NGLY1_DMSO.rda"))
 
