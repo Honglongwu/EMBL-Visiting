546742eaf2b67df6625be4fc9822e924418ac3ba
hanice <sunhanice@gmail.com>
2014-07-29 10:30:04 +0200

    hanice

---
+++ Code-SVN/ngly1/DE/de_exon_all-plot.R
@@ -0,0 +1,30 @@
+# test difference at exon level for all data
+# Author: czhu
+###############################################################################
+library(DEXSeq)
+
+folder = "/g/steinmetz/wmueller/NGLY1"
+outfolder = file.path(folder, "exon-CP4")
+
+load(file.path(outfolder, "DE_all.rda"))
+
+sigplot=function(gene){
+plotFolder=file.path(outfolder, "plot")
+if (!file.exists(plotFolder))  dir.create(plotFolder)
+
+pdf(file.path(plotFolder, paste0("DE_all_",paste(gene[1:3],collapse='_'),".pdf")), width=12, height=8)
+for(thisExon in gene){
+    plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE, 
+        fitExpToVar="treatment",norCounts=TRUE, splicing=TRUE)
+}
+dev.off()
+}
+
+sigplot('MYC')
+sigplot('TP53')
+sigplot('NGLY1')
+sigplot('NGLY1')
+sigplot('NGLY1')
+sigplot('NGLY1')
+sigplot('NGLY1')
+sigplot('NGLY1')
