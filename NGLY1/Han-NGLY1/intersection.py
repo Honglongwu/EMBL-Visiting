0804717391d22c0e870e2d5794b6e4a3c1194f7e
hanice <sunhanice@gmail.com>
2014-07-30 18:00:04 +0200

    hanice

--- NGLY1/Han-NGLY1/intersection.py
+++ NGLY1/Han-NGLY1/intersection.py
@@ -1,22 +1,21 @@
 inFile = open('UPF1-regulated.txt')
 D = {}
+line = inFile.readline()
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    fs =(fields[4]).strip().split(';')
-    for x in fs:
-        D[x] = 1
+    if float(fields[5]) > 0:
+        fs =(fields[4]).strip().split(';')
+        for x in fs:
+            D[x] = 1
 inFile.close()
 
-inFile = open('de.txt')
+
+inFile = open('DE_treatment_six_samples.txt')
 line = inFile.readline()
 for line in inFile:
     line = line.strip()
     fields = line.split()
-    try:
-        if float(fields[6]) <0.01:
-            if fields[7] in D:
-                print(line)
-    except:
-        pass
+    if fields[1] in D:
+        print(line)
 inFile.close()
