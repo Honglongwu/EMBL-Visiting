46182cee2a161584c489ccf346d01d991a3f2195
hanice <sunhanice@gmail.com>
2014-07-30 12:30:03 +0200

    hanice

--- Code-SVN/ngly1/DE/de_exon_NGLY1-CP2CP3FCP1.R
+++ Code-SVN/ngly1/DE/de_exon_NGLY1-CP2CP3FCP1.R
@@ -21,11 +21,9 @@ selGene = unique(as.character(exonicParts$gene_id))
 sel = as.logical(mcols(exonicParts)$gene_id %in% selGene)
 mygenes = exonicParts[sel]
 mat = mat[sel,]
-sampleAnnot$biorep = factor(sampleAnnot$biorep)
 
 
 dxd = DEXSeqDataSet(mat, sampleAnnot,
-    #design= ~individual + exon + sampleStatus:exon + biorep:exon,
     design= ~individual + exon + sampleStatus:exon,
     featureID=as.character(mcols(mygenes)$exonic_part), 
     groupID=as.character(mcols(mygenes)$gene_id),
@@ -36,9 +34,8 @@ dxd = DEXSeqDataSet(mat, sampleAnnot,
 ncpu=10
 dxd = DEXSeq::estimateSizeFactors( dxd )
 dxd = DEXSeq::estimateDispersions( dxd , BPPARAM=MulticoreParam(workers=ncpu))
-#dxd = testForDEU( dxd,reducedModel=~individual +exon+ biorep:exon,BPPARAM= MulticoreParam(workers=ncpu))
 dxd = testForDEU( dxd, reducedModel =~individual + exon,BPPARAM= MulticoreParam(workers=ncpu))
-dxd = estimateExonFoldChanges( dxd,fitExpToVar="condition", BPPARAM=MulticoreParam(workers=ncpu))
+dxd = estimateExonFoldChanges( dxd,fitExpToVar="sampleStatus", BPPARAM=MulticoreParam(workers=ncpu))
 dxr1 = DEXSeqResults( dxd )
 save(dxd, dxr1, file=file.path(outfolder, "DE_NGLY1_DMSO-CP2CP3FCP1.rda"))
 
