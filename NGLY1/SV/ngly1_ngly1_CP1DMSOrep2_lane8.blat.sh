d9837b2c9bfebee1d0da4a838ad73ab4d17fe1d5
hanice <sunhanice@gmail.com>
2014-08-04 13:30:05 +0200

    hanice

---
+++ NGLY1/SV/ngly1_ngly1_CP1DMSOrep2_lane8.blat.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd /g/steinmetz/hsun/NGLY1/SV
+db=/g/steinmetz/hsun/NGLY1/Genome/Homo_sapiens.GRCh37.68.dna.chromosomes.withIVTs.viral.fa
+query=ngly1_ngly1_CP1DMSOrep2_lane8.fasta
+out=${query}.blated
+blat $db $query -out=blast8 $out
