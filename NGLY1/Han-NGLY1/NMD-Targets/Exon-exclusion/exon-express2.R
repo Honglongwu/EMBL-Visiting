658eaf4ccd4608f33ac28260c93e94fb3b309ea8
hanice <sunhanice@gmail.com>
2014-08-05 09:30:02 +0200

    hanice

--- NGLY1/Han-NGLY1/NMD-Targets/Exon-exclusion/exon-express2.R
+++ NGLY1/Han-NGLY1/NMD-Targets/Exon-exclusion/exon-express2.R
@@ -1,4 +1,5 @@
 folder='/g/steinmetz/wmueller/NGLY1/exon-CP4'
+plotFolder = '/g/steinmetz/hsun/NGLY1/Han-NGLY1/NMD-Targets/Exon-exclusion/plot'
 get_exon_count=function(inFile)
 {
 load(file.path(folder,inFile))
@@ -8,29 +9,22 @@ load(file.path(folder,inFile))
 #exon=as.matrix(dxr1[dxr1$exonBaseMean==0,cn])
 #exon=rownames(dxr1)[dxr1$exonBaseMean<=COUNT]
 ##rownames(dxr1[dxr1$padj<0.01 & !is.na(dxr1$padj),])
-rownames(dxr1[dxr1$padj<0.01 & !is.na(dxr1$padj),])
+sig=dxr1[dxr1$padj<0.01 & !is.na(dxr1$padj),]
+sig_count = sig$countData
+sig_exclusion = (sig_count[,1] <=0 & sig_count[,2]) <=0 | (sig_count[,3] <= 0 & sig_count[,4] <= 0)
+for(gene in unqie(sig$groupID))
+{
+filename = paste0(gene,'.',strsplit(inFile,'[.]')[[1]][1],'.pdf')
+pdf(file.path(plotFolder, filename), width=12, height=8)
+for(thisExon in gene){
+        plotDEXSeq( dxr1, thisExon, legend=TRUE, cex.axis=1.2, cex=1.3, lwd=2 ,displayTranscripts=TRUE,
+                           fitExpToVar="treatment",norCounts=TRUE, splicing=TRUE)
+}
+dev.off()
+}
 
 #write.table(exon,ouFile,quote=F,col.names=F,row.names=F)
 }
 sample = c('DE_19.rda','DE_CP1.rda','DE_CP2.rda',
 'DE_CP3.rda','DE_CP4.rda','DE_MCP1.rda','DE_FCP1.rda')
-exon_unique = unique(unlist(lapply(sample,get_exon_count)))
-
-get_exon_count=function(inFile,exon_unique)
-{
-load(file.path(folder,inFile))
-#ouFile = paste0(strsplit(inFile,'[.]')[[1]][1],'.exon.txt')
-#cn=setdiff(colnames(dxr1),'genomicData')
-#cn=c('groupID','featureID')
-#exon=as.matrix(dxr1[dxr1$exonBaseMean==0,cn])
-exon_count=dxr1[exon_unique,]$countData
-n = strsplit(inFile,'[_|.]')[[1]][2]
-colnames(exon_count)=paste0(n,c('_AzaC_rep1','_AzaC_rep2','_DMSO_rep1','_DMSO_rep2'))
-return(exon_count)
-
-#write.table(exon,ouFile,quote=F,col.names=F,row.names=F)
-}
-exon_count=lapply(sample,get_exon_count,exon_unique=exon_unique)
-df=data.frame(exon_count)
-write.table(df,'sig_exon_count.txt',quote=F,col.names=NA)
-
+#exon_unique = unique(unlist(lapply(sample,get_exon_count)))
