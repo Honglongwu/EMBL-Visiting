3c41107fbce7a69dfb0ae48dfe881a5b155c806d
hanice <sunhanice@gmail.com>
2014-08-06 15:00:03 +0200

    hanice

---
+++ NGLY1/Han-NGLY1/NMD-Targets/Exon-exclusion/exon-exclusion-inclusion2.R
@@ -0,0 +1,37 @@
+library(DEXSeq)
+folder='/g/steinmetz/wmueller/NGLY1/drug-effect-pairwise-ExonLevel'
+plotFolder = '/g/steinmetz/hsun/NGLY1/Han-NGLY1/NMD-Targets/Exon-exclusion/plot'
+COUNT_MIN = 0
+COUNT_MAX = 20
+get_exon_count=function(inFile)
+{
+load(file.path(folder,inFile))
+sig=dxr1[dxr1$padj<0.01 & !is.na(dxr1$padj),]
+sig_count = sig$countData
+sig_exclusion = sig[(sig_count[,1] <=COUNT_MIN & sig_count[,2] <= COUNT_MIN) & (sig_count[,3] >= COUNT_MAX & sig_count[,4] >= COUNT_MAX),]
+sig_inclusion = sig[(sig_count[,3] <=COUNT_MIN  & sig_count[,4] <= COUNT_MIN ) & (sig_count[,1] >= COUNT_MAX & sig_count[,2] >=COUNT_MAX),]
+sig_exclusion_gene = sig_exclusion$groupID
+sig_inclusion_gene = sig_inclusion$groupID
+sig_exclusion_exon = ranges(sig_exclusion$genomicData)
+sig_inclusion_exon = ranges(sig_inclusion$genomicData)
+}
+
+get_element=function(x, i)
+{
+    return(x[i])
+}
+
+sample = c('de_19.rda','de_CP1.rda','de_CP2.rda',
+'de_CP3.rda','de_CP4.rda','de_MCP1.rda','de_FCP1.rda')
+#exon_unique = unique(unlist(lapply(sample,get_exon_count)))
+gene=lapply(sample,get_exon_count)
+exclusion_gene = unique(unlist(lapply(gene, get_element,i=1)))
+inclusion_gene = unique(unlist(lapply(gene, get_element,i=2)))
+load('/g/steinmetz/wmueller/NGLY1/gtf.rda')
+exclusion_gene_id = ids[ids$gene_name %in% exclusion_gene,c('gene_id','gene_name')]
+inclusion_gene_id = ids[ids$gene_name %in% inclusion_gene,c('gene_id','gene_name')]
+
+write.table(exclusion_gene_id,'de_exon_exclusion_gene.txt', row.names=F, col.names=F,quote=F)
+write.table(inclusion_gene_id,'de_exon_inclusion_gene.txt',row.names=F, col.names=F,quote=F)
+
+
--- NGLY1/Han-NGLY1/NMD-Targets/Exon-exclusion/exon-express.py
+++
@@ -1,7 +0,0 @@
-inFile = open('exonBaseMean-ALL-sample.txt')
-for line in inFile:
-    line = line.strip()
-    fields = line.split()
-    if sum([float(x) for x in fields[1:]]) >= 50: 
-        print(line)
-inFile.close()
