2aec7260db870b9cbb544bd6df53c7189c4fbded
hanice <sunhanice@gmail.com>
2014-08-08 16:00:01 +0200

    hanice

--- Code-SVN/ngly1/DE/DESeq_treatment_effect.R
+++ Code-SVN/ngly1/DE/DESeq_treatment_effect.R
@@ -63,7 +63,7 @@ print(plotPCA(rld, intgroup=c("individual", "sampleStatus")))
 dev.off()
 
 pdf(file.path(outfolder, "plot_MA.pdf"), width=8, height=6)
-plotMA(res, alpha=0.01)
+plotMA(results(dds), alpha=0.01)
 dev.off()
 
 pdf(file.path(outfolder, "plot_dispEst.pdf"), width=8, height=6)
