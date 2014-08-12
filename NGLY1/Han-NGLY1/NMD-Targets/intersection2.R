1c922496e1e33955d3a98404844e46c0ba05db57
hanice <sunhanice@gmail.com>
2014-08-12 11:30:03 +0200

    hanice

--- NGLY1/Han-NGLY1/NMD-Targets/intersection2.R
+++ NGLY1/Han-NGLY1/NMD-Targets/intersection2.R
@@ -1,3 +1,7 @@
 x = read.table('upf_drug_effect_atleastwo.txt')[,1]
 y = read.table('upf_drug_effect_atleasttwo_id.txt')[,2]
 table(y)[table(y)>1]
+
+
+x = read.table('upf_drug_effect_six_samples.txt')[,1]
+y = read.table('upf_drug_effect_four_patients.txt')[,1]
