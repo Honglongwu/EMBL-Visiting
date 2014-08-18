bab4b014917049521513ca74cfcd21a226f8c090
hanice <sunhanice@gmail.com>
2014-08-18 13:30:03 +0200

    hanice

--- NGLY1/Han-NGLY1/NMD-Targets/sig-six-formated.R
+++ NGLY1/Han-NGLY1/NMD-Targets/sig-six-formated.R
@@ -1,8 +1,9 @@
-data = load('DESeq-treatment-control-pairwise.rda')
+load('DESeq-treatment-control-pairwise.rda')
 sig.gene.six = read.table('DE_treatment_six_samples.txt')[,1]
 get.sig = function(x)
 {
-    x[rownames(x) %in% sig.gene.six,]
-
-
+    y=x[rownames(x) %in% sig.gene.six,]
+    write.table(y,paste0(unique(x$indi),'.six.txt'),quote=F,col.names=NA)
 }
+
+lapply(de.pw.indi.list,get.sig)
