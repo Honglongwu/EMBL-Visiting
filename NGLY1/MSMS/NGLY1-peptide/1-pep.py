b297d60794853351b00b40bea2e2ce9bf3003419
hanice <sunhanice@gmail.com>
2014-08-21 15:00:03 +0200

    hanice

---
+++ NGLY1/MSMS/NGLY1-peptide/1-pep.py
@@ -0,0 +1,22 @@
+D = {}
+inFile = open('NGLY1-seq.fa')
+while True:
+    line1 = inFile.readline().strip()
+    line2 = inFile.readline().strip()
+    if line1:
+        D.setdefault(line2,[line1])
+    else:
+        break
+inFile.close()
+
+inFile = open('../rep12/evidence.txt')
+for line in inFile:
+    fields = line.split('\t')
+    pep=fields[0]
+    for k in  D:
+        if pep in k:
+            D[k].append([pep,line])
+inFile.close()
+for k in D:
+    if len(D[k]) > 1:
+        print(D[k])
