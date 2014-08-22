f15cbc8eebf3b5e2fb8cc557e8444a8464c03e3b
hanice <sunhanice@gmail.com>
2014-08-22 15:30:04 +0200

    hanice

---
+++ NGLY1/SNV/CP4_AzaC_20uM_biorep1.sh
@@ -0,0 +1,3 @@
+cd /g/steinmetz/hsun/NGLY1/SNV/
+samtools mpileup -ugDf /g/steinmetz/hsun/NGLY1/SNV/Homo_sapiens.GRCh37.68.dna.chromosomes.withIVTs.fa CP4_AzaC_20uM_biorep1.bam | bcftools view -bvcg - > CP4_AzaC_20uM_biorep1.raw.vcf 
+bcftools view CP4_AzaC_20uM_biorep1.raw.vcf |vcfutils.pl varFilter  > CP4_AzaC_20uM_biorep1.flt.vcf
