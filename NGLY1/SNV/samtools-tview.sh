ce332db10cc0e58fa6b11bbc22343612e632c898
hanice <sunhanice@gmail.com>
2014-08-23 11:30:05 +0200

    hanice

--- NGLY1/SNV/samtools-tview.sh
+++ NGLY1/SNV/samtools-tview.sh
@@ -40,30 +40,38 @@
 
 
 
-samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25775417     
-samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25775417     
-samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25775417     
-samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25775417     
-
-samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25761670    
-samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25761670    
-samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25761670    
-samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25761670    
-
-samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25775422   
-samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25775422   
-samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25775422   
-samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25775422   
-
-samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25761025  
-samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25761025  
-samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25761025  
-samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25761025  
-
-
+#samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25775417     
+#samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25775417     
+#samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25775417     
+#samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25775417     
+#
+#samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25761670    
+#samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25761670    
+#samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25761670    
+#samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25761670    
+#
+#samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25775422   
+#samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25775422   
+#samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25775422   
+#samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25775422   
+#
+#samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25761025  
+#samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25761025  
+#samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25761025  
+#samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25761025  
 
 
 
+
+samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25778897
+samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25778897
+samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25778897
+samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25778897
+
+samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25781219
+samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25781219
+samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25781219
+samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25781219
 
 
 
