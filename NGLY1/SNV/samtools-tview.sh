2984298b4b13ab683a84b2c971d30932a0bd52b2
hanice <sunhanice@gmail.com>
2014-08-23 13:00:03 +0200

    hanice

--- NGLY1/SNV/samtools-tview.sh
+++ NGLY1/SNV/samtools-tview.sh
@@ -63,26 +63,26 @@
 
 
 
-samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25778897
-samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25778897
-samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25778897
-samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25778897
-
-samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25781219
-samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25781219
-samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25781219
-samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25781219
-
-
+#samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25778897
+#samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25778897
+#samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25778897
+#samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25778897
+#
+#samtools tview CP4_AzaC_20uM_biorep1.bam $hg19 -p 3:25781219
+#samtools tview CP4_AzaC_20uM_biorep2.bam $hg19 -p 3:25781219
+#samtools tview CP4_DMSO_biorep1.bam $hg19 -p 3:25781219
+#samtools tview CP4_DMSO_biorep2.bam $hg19 -p 3:25781219
 
 
 
 
-#samtools tview FCP1_AzaC_20uM_biorep1.bam $hg19 -p 3:
-#samtools tview FCP1_AzaC_20uM_biorep2.bam $hg19 -p 3:
-#samtools tview FCP1_DMSO_biorep1.bam $hg19 -p 3:
-#samtools tview FCP1_DMSO_biorep2.bam $hg19 -p 3:
-#samtools tview MCP1_AzaC_20uM_biorep1.bam $hg19 -p 3:
-#samtools tview MCP1_AzaC_20uM_biorep2.bam $hg19 -p 3:
-#samtools tview MCP1_DMSO_biorep1.bam $hg19 -p 3:
-#samtools tview MCP1_DMSO_biorep2.bam $hg19 -p 3:
+
+
+samtools tview FCP1_AzaC_20uM_biorep1.bam $hg19 -p 3:25761670
+samtools tview FCP1_AzaC_20uM_biorep2.bam $hg19 -p 3:25761670    
+samtools tview FCP1_DMSO_biorep1.bam $hg19 -p 3:25761670    
+samtools tview FCP1_DMSO_biorep2.bam $hg19 -p 3:25761670    
+samtools tview MCP1_AzaC_20uM_biorep1.bam $hg19 -p 3:25775417
+samtools tview MCP1_AzaC_20uM_biorep2.bam $hg19 -p 3:25775417
+samtools tview MCP1_DMSO_biorep1.bam $hg19 -p 3:25775417
+samtools tview MCP1_DMSO_biorep2.bam $hg19 -p 3:25775417
