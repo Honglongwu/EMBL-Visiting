04820c005231a3d2647da026ffb63fc07e16bccc
hanice <sunhanice@gmail.com>
2014-08-25 14:30:03 +0200

    hanice

--- NGLY1/Data/Ensembl/1-isform.py
+++ NGLY1/Data/Ensembl/1-isform.py
@@ -10,6 +10,13 @@ for line in inFile:
         D.setdefault(transcript_id, [])
         D[transcript_id].append([exon_number, int(fields[3]), int(fields[4])])
 inFile.close()
+
+'''
+for k in D:
+    print(k)
+    print(D[k])
+'''
+
 def get_min_max(D):
     L = []
     for k in D:
@@ -19,15 +26,23 @@ def get_min_max(D):
     L.sort()
     return([L[0], L[-1], L[-1]-L[0]])
 
-GL = []
+GL = {}
 for k in D:
-    L = [0]*(get_min_max(D)[2])
+    L = [0]*(get_min_max(D)[2]+1)
     for exon in D[k]:
         for i in range(exon[1], exon[2]+1):
-            print(i)
-            L[i-get_min_max[0]] = 1
-    GL.append(L) 
+            L[i-get_min_max(D)[0]] = 1
+    GL[k] = L
 
-print(GL)
-
-
+GLx = GL.items()
+for i in range(len(GLx)):
+    L = []
+    for j in range(len(GLx[i][1])):
+        if GLx[i][1][j] == 1:
+            s = 0
+            for m in range(len(GLx)):
+                s += GLx[m][1][j]
+            if s == 1:
+                L.append(j+get_min_max(D)[0])
+    print(GLx[i][0])
+    print(L)
