eee2e80fabe2973a1d95120df2fca1a860ca4f60
hanice <sunhanice@gmail.com>
2014-08-29 15:00:04 +0200

    hanice

--- NGLY1/SV/2-blat.py
+++ NGLY1/SV/2-blat.py
@@ -3,6 +3,23 @@ n = 0
 m = 0
 L = []
 MAX = 100000
+
+
+def mkblat(filename):
+    ouFile = open(filename+'.blat.sh','w')
+    ouFile.write('#!/bin/bash\n')
+    ouFile.write('cd /g/steinmetz/hsun/NGLY1/SV\n')
+    ouFile.write('db=/g/steinmetz/hsun/NGLY1/Genome/Homo_sapiens.GRCh37.68.dna.chromosomes.withIVTs.viral.fa\n')
+    ouFile.write('query='+filename+'\n')
+    ouFile.write('out=${query}.blated\n')
+    ouFile.write('blat $db $query -out=blast8 $out\n')
+    ouFile.close()
+def runblat(filename):
+    subprocess.call(['bash',filename+'.blat.sh'])
+def filterblat(filename):
+    inFile = open(filename + '.blated')
+    inFile.close()
+
 while True:
     line1 = inFile.readline()
     line2 = inFile.readline()
@@ -13,20 +30,24 @@ while True:
             L.append(line2)
         else:
             m += 1
-            ouFile = open('test.'+str(m)+'.fa', 'w')
+            filename = 'test.'+str(m)+'.fa'
+            ouFile = open(filename, 'w')
             for x in L:
                 ouFile.write(x)
             ouFile.close()
-            n = 0
+            mkblat(filename)
             L = []
             L.append(line1)
             L.append(line2)
+            n = 1
     else:
         m += 1
-        ouFile = open('test.'+str(m)+'.fa', 'w')
+        filename = 'test.'+str(m)+'.fa'
+        ouFile = open(filename, 'w')
         for x in L:
             ouFile.write(x)
         ouFile.close()
+        mkblat(filename)
         break
 inFile.close()
 '''
