740b727173a8cdc06c3eb5dbe1ae0e7b4dbee5bc
hanice <sunhanice@gmail.com>
2014-09-04 13:30:03 +0200

    hanice

--- NGLY1/SV/2-bowtie2.sh
+++ NGLY1/SV/2-bowtie2.sh
@@ -1,1 +1,1 @@
-bowtie2 -p 8 -x /g/steinmetz/genome/Homo_sapiens/37.68/indexes/bowtie2/Homo_sapiens.GRCh37.68.withIVTs -U NGLY1-unmapped.fq -S NGLY1-unmapped.sam
+bowtie2 -p 8 -k 2 -x /g/steinmetz/genome/Homo_sapiens/37.68/indexes/bowtie2/Homo_sapiens.GRCh37.68.withIVTs -U NGLY1-unmapped.fq -S NGLY1-unmapped.sam
