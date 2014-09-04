57dc132bdb6074cd6a7d684378090cb73cffc6f7
hanice <sunhanice@gmail.com>
2014-09-04 15:00:04 +0200

    hanice

--- NGLY1/SV/3-bowtie2-filter5.py
+++ NGLY1/SV/3-bowtie2-filter5.py
@@ -1,5 +1,14 @@
+D = {}
+
+def similar(k, D):
+    for y in D:
+        x = y.split('\t')
+        if (x[0] == k[0] and 10<=int(x[1]) - int(k[1]) <=10 and x[2] == k[2] and 10 <= int(x[3]) - int(k[3]) <= 10) \
+                or (x[0] == k[2] and 10<=int(x[1]) - int(k[3]) <=10 and x[2] == k[0] and 10 <= int(x[3]) - int(k[1]) <= 10):
+                    return y
+    return 0
 inFile = open('NGLY1-unmapped.aligned.paired3')
-ouFile = open('NGLY1-unmapped.aligned.paired4')
+ouFile = open('NGLY1-unmapped.aligned.paired4','w')
 while True:
     line1 = inFile.readline().strip()
     line2 = inFile.readline().strip()
@@ -7,12 +16,21 @@ while True:
         fields1 = line1.split('\t')
         fields2 = line2.split('\t')
         ch1 = fields1[2]
-        pos1 = int(fields1[3])
+        pos1 = fields1[3]
         ch2 = fields2[2]
-        pos2 = int(fields2[3])
-
-
+        pos2 = fields2[3]
+        k = [ch1,pos1,ch2,pos2]
+        x = similar(k, D)
+        if x:
+            D[x].append([line1, line2])
+        else:
+            D.setdefault('\t'.join(k),[[line1, line2]])
     else:
         break
 inFile.close()
+for k in D:
+    if len(D[k]) > 1:
+        ouFile.write(k+'\n')
+        for x in D[k]:
+            ouFile.write('\n'.join(x)+'\n')
 ouFile.close()
