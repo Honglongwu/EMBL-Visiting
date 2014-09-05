131810c8949cffbe89e1a11239f1a87cf9da925d
hanice <sunhanice@gmail.com>
2014-09-05 12:00:05 +0200

    hanice

---
+++ NGLY1/SV/4-filter-2.py
@@ -0,0 +1,26 @@
+D = {}
+inFile = open('NGLY1-unmapped.aligned.paired4')
+while True:
+    line = inFile.readline().strip()
+    if line:
+        if line[0] == '>':
+            head = '_'.join(line.split('\t'))
+            D.setdefault(head, [])
+        else:
+            D[head].append(line)
+    else:
+        break
+inFile.close()
+
+inFile = open('NGLY1-unmapped.aligned.paired6.fa.blated.filtered')
+ouFile = open('NGLY1-unmapped.aligned.paired6.fa.blated.filtered.num', 'w')
+for line in inFile:
+    fields = line.split('\t')
+    for k in D:
+        if fields[0] in k:
+            fds = k[1:].split('_')
+            n = fds[0]
+            ouFile.write(n+'\t' + line)
+            break
+inFile.close()
+ouFile.close()
