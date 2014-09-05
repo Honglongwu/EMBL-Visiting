98728507df4cc8c65caf16174a1e7e5f33c52ff5
hanice <sunhanice@gmail.com>
2014-09-05 14:30:04 +0200

    hanice

--- NGLY1/SNV/test.sh
+++ NGLY1/SNV/test.sh
@@ -1,4 +1,4 @@
 cd /g/steinmetz/hsun/NGLY1/SNV/
 #perl /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/convert2annovar.pl --format vcf4 --includeinfo MCP1_AzaC_20uM_biorep2.flt.vcf >MCP1_AzaC_20uM_biorep2.flt.annovar
 #perl /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/annotate_variation.pl -geneanno MCP1_AzaC_20uM_biorep2.flt.annovar  /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/humandb/ --buildver hg19
-perl /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/annotate_variation.pl  -filter -dbtype 1000g2012apr_all -maf 0.01 test /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/humandb/ --buildver hg19
+perl /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/annotate_variation.pl  -filter -buildver hg19 -dbtype snp138   test /g/steinmetz/hsun/MySoft/ANNOVAR/annovar/humandb/ 
