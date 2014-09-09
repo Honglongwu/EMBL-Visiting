732cc1f15f120fa785c3e50e27b0a513e520c10f
hanice <sunhanice@gmail.com>
2014-09-09 14:00:02 +0200

    hanice

--- NGLY1/Annotation/Isoforms/5-unique.py
+++ NGLY1/Annotation/Isoforms/5-unique.py
@@ -4,7 +4,30 @@ while True:
     line2 = inFile.readline().strip()
     if line1:
         gene = line1.split('"')[1]
-        print(gene)
+        fields = line2.split('\t')
+        min = int(fields[0])
+        max = int(fields[-1])
+        i = min
+        while i <= max:
+            if str(i) in fields:
+                start = i
+                j = i+1
+                while j <= max:
+                    if j == max:
+                        end = j 
+                        print(start)
+                        print(end)
+                        i = j + 1
+                        break
+                    elif str(j) not in fields:
+                        end = j - 1
+                        print(start)
+                        print(end)
+                        i = j + 1
+                        break
+                    else:
+                        j += 1
+                        
     else:
         break
 inFile.close()
