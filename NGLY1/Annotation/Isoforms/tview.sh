3a99ff6580fe086fc42b9b494bb9e2a7796661a8
hanice <sunhanice@gmail.com>
2014-09-10 11:00:05 +0200

    hanice

--- NGLY1/Annotation/Isoforms/tview.sh
+++ NGLY1/Annotation/Isoforms/tview.sh
@@ -1,29 +1,18 @@
-#ENST00000489271 $hg19
-samtools tview $1 -p chr3:25770810 $hg19
-samtools tview $1 -p chr3:25772041  $hg19
-#ENST00000463611 $hg19
-samtools tview $1 -p chr3:25824384 $hg19
-samtools tview $1 -p chr3:25824750  $hg19
-#ENST00000478991 $hg19
-samtools tview $1 -p chr3:25819753 $hg19
-samtools tview $1 -p chr3:25820064  $hg19
-#ENST00000280700 $hg19
-samtools tview $1 -p chr3:25824990 $hg19
-samtools tview $1 -p chr3:25825042  $hg19
-#ENST00000496726 $hg19
-samtools tview $1 -p chr3:25775476 $hg19
-samtools tview $1 -p chr3:25777494 $hg19
-samtools tview $1 -p chr3:25777641 $hg19
-samtools tview $1 -p chr3:25777799  $hg19
-#ENST00000493324 $hg19
-samtools tview $1 -p chr3:25777892 $hg19
-samtools tview $1 -p chr3:25778824  $hg19
-#ENST00000427041 $hg19
-samtools tview $1 -p chr3:25824229 $hg19
-samtools tview $1 -p chr3:25824363  $hg19
-#ENST00000461491 $hg19
-samtools tview $1 -p chr3:25825534 $hg19
-samtools tview $1 -p chr3:25825647  $hg19
 #ENST00000417874 $hg19
-samtools tview $1 -p chr3:25831352 $hg19
-samtools tview $1 -p chr3:25831530  $hg19
+samtools tview $1 -p 3:25831352 $hg19
+#ENST00000280700 $hg19
+samtools tview $1 -p 3:25824990 $hg19
+#ENST00000427041 $hg19
+samtools tview $1 -p 3:25824229 $hg19
+#ENST00000493324 $hg19
+samtools tview $1 -p 3:25777892 $hg19
+#ENST00000489271 $hg19
+samtools tview $1 -p 3:25770810 $hg19
+#ENST00000496726 $hg19
+samtools tview $1 -p 3:25775476 $hg19
+#ENST00000478991 $hg19
+samtools tview $1 -p 3:25819753 $hg19
+#ENST00000463611 $hg19
+samtools tview $1 -p 3:25824384 $hg19
+#ENST00000461491 $hg19
+samtools tview $1 -p 3:25825534 $hg19
