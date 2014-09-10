86603821e684f06d59c5bbf87827eabcc3585f18
hanice <sunhanice@gmail.com>
2014-09-10 16:00:06 +0200

    hanice

---
+++ NGLY1/Annotation/Isoforms/get_region_read_count-CP1234.R
@@ -0,0 +1,12 @@
+library(GenomicAlignments)
+fls <- list.files('/g/steinmetz/hsun/NGLY1/NGLY1-wmueller/alignment_filtered',
+                   recursive=TRUE, pattern="*bam$", full=TRUE)
+bfl <- BamFileList(fls)
+
+features = GRanges(
+seqnames = rep('3', times = 11),
+ranges = IRanges(start = c(25760435,25770810,25824384,25819753,25824990,25775476,25777641,25777892,25824229,25825534,25831352), end = c(25760435,25772041,25824750,25820064,25825042,25777494,25777799,25778824,25824363,25825647,25831530)),'-',
+group_id=c('ENST00000489271','ENST00000489271','ENST00000463611','ENST00000478991','ENST00000280700','ENST00000496726','ENST00000496726','ENST00000493324','ENST00000427041','ENST00000461491','ENST00000417874')
+)
+olap <- summarizeOverlaps(features, bfl, ignore.strand=TRUE, singleEnd = TRUE)
+save(assay(olap), 'ENSG00000151092-GRCh37-transcripts-unique-region2-counts-CP1234.rda')
