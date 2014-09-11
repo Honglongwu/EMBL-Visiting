686a9fd5ff18801ca474d99d90cfb45f21a49984
hanice <sunhanice@gmail.com>
2014-09-11 15:00:02 +0200

    hanice

--- NGLY1/Annotation/Isoforms/get_region_read_count-BodyMap.R
+++ NGLY1/Annotation/Isoforms/get_region_read_count-BodyMap.R
@@ -9,4 +9,4 @@ ranges = IRanges(start = c(25760435,25770810,25824384,25819753,25824990,25775476
 group_id=c('ENST00000489271','ENST00000489271','ENST00000463611','ENST00000478991','ENST00000280700','ENST00000496726','ENST00000496726','ENST00000493324','ENST00000427041','ENST00000461491','ENST00000417874')
 )
 count <- assay(summarizeOverlaps(features, bfl, ignore.strand=TRUE))
-save(count, file='ENSG00000151092-GRCh37-transcripts-unique-region2-counts.rda')
+save(count, file='ENSG00000151092-GRCh37-transcripts-unique-region2-counts-BodyMap.rda')
