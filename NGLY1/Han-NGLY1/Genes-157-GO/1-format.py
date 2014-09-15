bd4c267de1f9d904e8030dfeb4df2900c9c1a145
hanice <sunhanice@gmail.com>
2014-09-15 10:30:04 +0200

    hanice

--- NGLY1/Han-NGLY1/Genes-157-GO/1-format.py
+++ NGLY1/Han-NGLY1/Genes-157-GO/1-format.py
@@ -1,2 +1,20 @@
-inFile = open()
+import re
+inFile = open('GOPROCESS.txt')
+ouFile = open('NGLY1-sig-GOPROCESS.txt', 'w')
+header = inFile.readline()
+D = {}
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    k = fields[1] + ' (' + fields[0] + ',padj=' + fields[3] + ',' + 'N=' + fields[8] + ')'
+    s=re.findall('(\w+)  -  ', fields[9])
+    if s:
+        D[k] = s
 inFile.close()
+
+d = D.items()
+d.sort(lambda x,y:cmp(len(x[1]), len(y[1])), reverse=True)
+for x in d:
+    if len(x[1]) > 10:
+        ouFile.write(x[0] + '\t' + '\t'.join(x[1]) + '\n')
+ouFile.close()
