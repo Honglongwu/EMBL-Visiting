5af1b39d7c2f9232ad35ec21ec5388f4c85eb7f6
hanice <sunhanice@gmail.com>
2014-09-27 18:30:02 +0200

    hanice

--- NGLY1/Annotation/Isoforms/Resutls-from-BodyMap/1-format.py
+++ NGLY1/Annotation/Isoforms/Resutls-from-BodyMap/1-format.py
@@ -4,6 +4,7 @@ D.setdefault('Expression', [])
 D.setdefault('Others', [])
 
 inFile = open('NGLY1-BodyMap-Transcripts.txt')
+ouFile = open('NGLY1-BodyMap-Transcripts-formated', 'w')
 line = inFile.readline().strip().split('\t')
 D['Tissue'] = line
 line = inFile.readline().strip().split('\t')
@@ -19,5 +20,6 @@ for line in inFile:
         D[x][i] = 1
 inFile.close()
 for k in D:
-    print(k + '\t' + '\t'.join([str(x) for x in D[k]]))
+    ouFile.write(k + '\t' + '\t'.join([str(x) for x in D[k]]) + '\n')
+ouFile.close()
 
---
+++ NGLY1/Annotation/Isoforms/Resutls-from-BodyMap/2-heatmap.R
@@ -0,0 +1,2 @@
+rt=read.table("NGLY1-BodyMap-Transcripts-formated", header=T,sep="\t")
+rt.sub=rt[2:dim(rt)[1],]
