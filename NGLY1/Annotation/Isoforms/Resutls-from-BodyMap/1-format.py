bb623f670226e61e1f1655c568e6638290df2094
hanice <sunhanice@gmail.com>
2014-09-27 18:00:04 +0200

    hanice

--- NGLY1/Annotation/Isoforms/Resutls-from-BodyMap/1-format.py
+++ NGLY1/Annotation/Isoforms/Resutls-from-BodyMap/1-format.py
@@ -1,4 +1,23 @@
 D = {}
+D.setdefault('Tissue', [])
+D.setdefault('Expression', [])
+D.setdefault('Others', [])
 
 inFile = open('NGLY1-BodyMap-Transcripts.txt')
+line = inFile.readline().strip().split('\t')
+D['Tissue'] = line
+line = inFile.readline().strip().split('\t')
+D['Expression'] = [int(x) for x in line]
+line = inFile.readline().strip().split('\t')
+D['Others'] = [int(x)-3 for x in line]
+
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    for i,x in enumerate(fields):
+        D.setdefault(x, [0]*len(D['Tissue']))
+        D[x][i] = 1
 inFile.close()
+for k in D:
+    print(k + '\t' + '\t'.join([str(x) for x in D[k]]))
+
