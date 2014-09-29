302534375540aea00af4e92963231178d0d6afcd
hanice <sunhanice@gmail.com>
2014-09-30 01:30:01 +0200

    hanice

---
+++ NGLY1/Virus/NGLY1-unmapped-bam-fasta-BCells/9-plot.R
@@ -0,0 +1,14 @@
+library(ggplot2)
+gg_color_hue <- function(n) {
+      hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
+rt =read.table('NGLY1-Viruses-samples2')
+colnames(rt) = c('Sample','Count')
+virus = rep('human herpesvirus 4 (EBV)',times=dim(rt)[1])
+data=cbind(rt,virus)
+#qplot(x=ordered(as.factor(Sample),c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), y='Reads Count', data=rt, geom="bar", stat="identity",position="dodge", fill="white",colour="darkgreen")
+#qplot(x=Sample, y=Count, data=rt, geom="bar", stat="identity",position="dodge")
+pdf("NGLY1-Virus-B-Cells2.pdf")
+qplot(x=factor(Sample, levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), y=Count, fill=virus,data=data, geom="bar", stat="identity",position="dodge",xlab="Sample of B cells", ylab="Number of reads")
+dev.off()
