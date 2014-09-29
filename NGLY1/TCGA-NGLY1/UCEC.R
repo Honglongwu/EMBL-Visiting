c4f67d82a5e1efab890012e77b20ff65c2ab786c
hanice <sunhanice@gmail.com>
2014-09-29 14:30:02 +0200

    hanice

--- NGLY1/TCGA-NGLY1/UCEC.R
+++ NGLY1/TCGA-NGLY1/UCEC.R
@@ -50,27 +50,27 @@ NGLY1.count.t.ga = subset(NGLY1.count.t,annotation$platform=="ga")
 selection = NGLY1.count.t<=600 & NGLY1.count.t >= 500 & annotation$platform=='ga' & annotation$tumor_status=='TUMOR FREE' & annotation$race=='WHITE' & annotation$age <= 70 & annotation$age >= 50
 control=count[,selection]
 control.annotation = annotation[selection,]
-
+write.table(control,file="UCEC-36control",quote=F,col.names=NA)
 ###ngly1=count[,grepl("TCGA-D1-A17Q-01|TCGA-B5-A0JY-01|TCGA-D1-A103-01|TCGA-B5-A11N-01",colnames(count))]
 ngly1=count[,grepl("TCGA-D1-A17Q-01|TCGA-B5-A0JY-01|TCGA-B5-A11N-01",colnames(count))]
 ngly1.annotation = annotation[annotation$sample %in% colnames(ngly1),]
-ngly1[grepl("NGLY1",rownames(ngly1)),]
+write.table(ngly1,file="UCEC-3NGLY1",quote=F,col.names=NA)
 
 ### merge ngly1 and control
-comparison = rep(c('ngly1','control'), times=c(dim(ngly1.annotation)[1], dim(control.annotation)[1]))
+condition = rep(c('ngly1','control'), times=c(dim(ngly1.annotation)[1], dim(control.annotation)[1]))
 ngly1.control = merge(ngly1, control, by.x=0, by.y=0)
 rownames(ngly1.control) = ngly1.control[,1]
 ngly1.control=ngly1.control[2:dim(ngly1.control)[2]]
-ngly1.control.annotation = cbind(rbind(ngly1.annotation, control.annotation),comparison)
-ngly1.control = merge(ngly1, control, by.x=1, by.y=1)
-save.image(file="UCEC.rda")
+ngly1.control.annotation = cbind(rbind(ngly1.annotation, control.annotation),condition)
+#ngly1.control = merge(ngly1, control, by.x=1, by.y=1)
 
 ### DESeq2
-gly1.control.tmp = sapply(ngly1.control, as.integer)
-rownames(ngly1.control.tmp)=rownames(ngly1.control)
-ngly1.control=ngly1.control.tmp
-dds=DESeqDataSetFromMatrix(countData=ngly1.control, colData=ngly1.control.annotation,design=~comparison)
+#ngly1.control.tmp = sapply(ngly1.control, as.integer)
+#rownames(ngly1.control.tmp)=rownames(ngly1.control)
+#ngly1.control=ngly1.control.tmp
+dds=DESeqDataSetFromMatrix(countData=ngly1.control, colData=ngly1.control.annotation,design=~condition)
 dds <- DESeq(dds)
 dds.results=results(dds)
 dds.results=dds.results[order(dds.results$padj),]
 dds.results.significant=[which(dds.results$padj<0.05),]
+save.image(file="UCEC.rda")
