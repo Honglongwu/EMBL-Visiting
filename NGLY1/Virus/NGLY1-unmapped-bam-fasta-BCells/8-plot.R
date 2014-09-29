1042a406c46ea183753c3848ae2d37374fb91ea1
hanice <sunhanice@gmail.com>
2014-09-29 16:30:04 +0200

    hanice

--- NGLY1/Virus/NGLY1-unmapped-bam-fasta-BCells/8-plot.R
+++ NGLY1/Virus/NGLY1-unmapped-bam-fasta-BCells/8-plot.R
@@ -1,5 +1,4 @@
 library(ggplot2)
 rt =read.table('NGLY1-Viruses-samples2')
-colnames(rt) = c('sample','count')
-ggplot(rt$count, aes(factor(count)))
-c + geom_bar()
+colnames(rt) = c('Sample','Reads Count')
+qplot(x=ordered(as.factor(sample),c()), y=count, data=rt, geom="bar", stat="identity",position="dodge", fill="white",colour="darkgreen")
