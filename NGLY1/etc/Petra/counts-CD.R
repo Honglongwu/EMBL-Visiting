ad1b73ee75f26b7044264ff38a101bf99728acf0
hanice <sunhanice@gmail.com>
2014-10-01 10:00:04 +0200

    hanice

--- NGLY1/etc/Petra/counts-CD.R
+++ NGLY1/etc/Petra/counts-CD.R
@@ -6,3 +6,5 @@ colnames(rt) = c("Symbol","ID")
 load('/g/steinmetz/hsun/NGLY1/NGLY1-wmueller/counts-BCells.rda')
 data = assay(geneCounts)
 
+CD=data[rownames(data) %in% rt$ID,]
+write.table(CD,file='CDx-reads-count',quote=F,col.names=NA)
