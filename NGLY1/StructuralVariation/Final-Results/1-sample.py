899d66e8f58d1fbe0ba75852cedfb2ed13c02110
hanice <sunhanice@gmail.com>
2014-10-04 17:30:03 +0200

    hanice

--- NGLY1/StructuralVariation/Final-Results/1-sample.py
+++ NGLY1/StructuralVariation/Final-Results/1-sample.py
@@ -1,3 +1,4 @@
+import re
 inFile = open('NGLY1-Structural-Variatioin-Candidates-cut5')
 ouFile = open('NGLY1-Structural-Variatioin-Candidates-cut5-sample', 'w')
 D = {}
@@ -20,8 +21,37 @@ while True:
                 Type = 'UTR'
             else:
                 Type = 'INTRON'
-            print(gene + '\t' + Type)
+            #print(gene + '\t' + Type)
             D.setdefault(gene, [])
+            fields = line3.split('\t')
+            for fd in fields:
+                x = fd.split('|')
+                for y in x:
+                    s = y.split(':')[0]
+                    sr = re.search('_(.*?CP.*?)_',s)
+                    if sr:
+                        ss=sr.group(1)
+                        if ss.find('ngly1_') !=-1:
+                            if ss.find('FCP1') != -1:
+                                ss = 'FCP1'
+                            elif ss.find('MCP1') != -1:
+                                ss = 'MCP1'
+                            elif ss.find('CP1') != -1:
+                                ss = 'CP1'
+                            elif ss.find('CP2') != -1:
+                                ss = 'CP2'
+                            elif ss.find('CP3') != -1:
+                                ss = 'CP3'
+                            elif ss.find('CP4') != -1:
+                                ss = 'CP4'
+                        print(ss)
+ 
+                                
+ 
+ 
+ 
+ 
+                
     else:
         break
 
