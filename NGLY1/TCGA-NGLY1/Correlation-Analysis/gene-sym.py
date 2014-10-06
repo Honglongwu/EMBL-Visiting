b1ae46a8924c0e3040face13812ebbf97a1fceed
hanice <sunhanice@gmail.com>
2014-10-06 11:30:02 +0200

    hanice

--- NGLY1/TCGA-NGLY1/Correlation-Analysis/gene-sym.py
+++ NGLY1/TCGA-NGLY1/Correlation-Analysis/gene-sym.py
@@ -3,6 +3,7 @@ inFile = open(sys.argv[1])
 ouFile = open(sys.argv[1]+'.gene', 'w')
 for line in inFile:
     fields = line.split('|')
-    ouFile.write(fields[0] + '\n')
+    if fields[0] != '?':
+        ouFile.write(fields[0] + '\n')
 inFile.close()
 ouFile.close()
