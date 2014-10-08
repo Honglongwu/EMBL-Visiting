3c038e220082cccdb1704504908494abbcce02c3
hanice <sunhanice@gmail.com>
2014-10-08 11:00:02 +0200

    hanice

---
+++ NGLY1/GeneCheck/gene-expression-plot-NGLY1.R
@@ -0,0 +1,10 @@
+folder = '/g/steinmetz/hsun/NGLY1/NGLY1-wmueller'
+load(file.path(folder,'counts-BCells.rda'))
+load(file.path(folder,'gtf.rda'))
+load(file.path(folder,'sampleAnnot-BCells.rda'))
+
+library(DESeq2)
+mat=assay(geneCounts)
+pdf('BCells-NGLY1-Expression.pdf')
+lattice::dotplot(mat[ids[ids$gene_name == 'NGLY1',]$gene_id,]~factor(sampleAnnot$individual,levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NGLY1")
+dev.off()
