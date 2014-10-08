5f64fd32f5adfbcdd89653519766b46efc8d8f1d
hanice <sunhanice@gmail.com>
2014-10-08 15:30:04 +0200

    hanice

--- NGLY1/GeneCheck/gene-expression-plot-UCEC.R
+++ NGLY1/GeneCheck/gene-expression-plot-UCEC.R
@@ -1,10 +1,18 @@
 folder = '/g/steinmetz/hsun/NGLY1/TCGA-NGLY1'
 load(file.path(folder,'UCEC.rda'))
-load(file.path(folder,'gtf.rda'))
+load('/g/steinmetz/hsun/NGLY1/NGLY1-wmueller/gtf.rda')
 
 library(DESeq2)
-mat=assay(geneCounts)
+mat=ngly1.control
 pdf('UCEC-NPC1-DYNC1H1-Expression.pdf')
-lattice::dotplot(mat[ids[ids$gene_name == 'NPC1',]$gene_id,]~factor(sampleAnnot$individual), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NPC1")
-lattice::dotplot(mat[ids[ids$gene_name == 'DYNC1H1',]$gene_id,]~factor(sampleAnnot$individual), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="DYHC1H1")
+lattice::dotplot(as.matrix(mat)[grepl('^NPC1\\|',rownames(mat)),]~ngly1.control.annotation$condition, group=ngly1.control.annotation$condition, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NPC1")
+
+lattice::dotplot(as.matrix(mat)[grepl('^DYCN1H1\\|',rownames(mat)),]~ngly1.control.annotation$condition, group=ngly1.control.annotation$condition, auto.key=TRUE,pch=19, ylab="Gene Expression",main="DYCN1H1")
+dev.off()
+
+
+pdf('UCEC-NPC1-DYNC1H1-Normalized-Expression.pdf')
+lattice::dotplot(as.matrix(mat)[grepl('^NPC1\\|',rownames(mat)),]~ngly1.control.annotation$condition, group=ngly1.control.annotation$condition, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NPC1")
+
+lattice::dotplot(as.matrix(mat)[grepl('^DYCN1H1\\|',rownames(mat)),]~ngly1.control.annotation$condition, group=ngly1.control.annotation$condition, auto.key=TRUE,pch=19, ylab="Gene Expression",main="DYCN1H1")
 dev.off()
