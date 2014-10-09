81a3a81d19035f956405d1e57395904f859c130d
hanice <sunhanice@gmail.com>
2014-10-09 14:00:04 +0200

    hanice

--- NGLY1/GeneCheck/gene-expression-plot-NGLY1.R
+++ NGLY1/GeneCheck/gene-expression-plot-NGLY1.R
@@ -5,6 +5,12 @@ load(file.path(folder,'sampleAnnot-BCells.rda'))
 
 library(DESeq2)
 mat=assay(geneCounts)
+dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~individual)
+dds = DESeq(dds)
+expr = counts(dds, normalized=T)
+expr.raw = counts(dds, normalized=F)
+
 pdf('BCells-NGLY1-Expression.pdf')
-lattice::dotplot(mat[ids[ids$gene_name == 'NGLY1',]$gene_id,]~factor(sampleAnnot$individual,levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NGLY1")
+lattice::dotplot(expr[ids[ids$gene_name == 'NGLY1',]$gene_id,]~factor(sampleAnnot$individual,levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Normalized Gene Expression",main="NGLY1")
+lattice::dotplot(expr.raw[ids[ids$gene_name == 'NGLY1',]$gene_id,]~factor(sampleAnnot$individual,levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression (Raw Counts)",main="NGLY1")
 dev.off()
