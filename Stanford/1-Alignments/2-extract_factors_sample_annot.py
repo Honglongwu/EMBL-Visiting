181374bc74fa053f2542cbb95b5a777a316cd5b5
hanice <sunhanice@gmail.com>
2015-01-16 21:00:03 +0100

    hanice

--- Stanford/1-Alignments/2-extract_factors_sample_annot.py
+++ Stanford/1-Alignments/2-extract_factors_sample_annot.py
@@ -2,6 +2,7 @@ def format(F):
     inFile = open(F)
     head = inFile.readline()
 
+    L = []
     for line in inFile:
         line = line.strip()
         fields = line.split('\t')
@@ -10,13 +11,35 @@ def format(F):
         filename = fields[2]
         lane = fields[3]
         name = fields[4]
+        if name.find('primary') != -1:
+            fds = name.split('_')
+            if fds[1][0:2] == 'WT':
+                sample = fds[1][0:-1]
+                biorep = 'biorep' + fds[1][-1]
+                passage = fds[2]
+                lane = fds[3]
+            elif fds[2] != 'NO': 
+                sample = fds[1] + '_' + fds[2][0:-1]
+                biorep = 'biorep' + fds[2][-1]
+                passage = fds[3]
+                lane = fds[4]
+            else:
+                sample = fds[1] + '_' + 'KO'
+                biorep = 'biorep1'
+                passage = fds[3]
+                lane = fds[4]
+        else:
+            fds = name.split('_')
+            sample = fds[1]
+            biorep = 'biorep' + fds[2]
+            passage = 'None'
+            lane = fds[3]
 
+        #print('\t'.join([sample, biorep, passage, lane]))
+        L.append([sample, biorep, passage, lane])
 
-
-
-
+            
         #print('\t'.join([sampleName, barcode, lane, name]))
-        print(name)
     inFile.close()
 
 
