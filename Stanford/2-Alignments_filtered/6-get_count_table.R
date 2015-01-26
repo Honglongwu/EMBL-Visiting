81a44bfe6fa26a91092ac8a9f8ac9ce6af48b639
hanice <sunhanice@gmail.com>
2015-01-26 21:30:03 +0100

    hanice

--- Stanford/2-Alignments_filtered/6-get_count_table.R
+++ Stanford/2-Alignments_filtered/6-get_count_table.R
@@ -39,19 +39,24 @@ exonCounts <- summarizeOverlaps( unlist(exonsByGene), bamLst,
     inter.feature=FALSE)
 
 ## pheno data
-pd1 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-2014-10-21.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
+pd1 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-human-2014-10-21.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
 #pd = pd[,c("individual", "treatment", "biorep", "techrep", "label")]
 pd1 = pd1[,c("sample", "biorep", "passage", "lane", "label")]
 
-pd2 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-2014-11-10.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
+pd2 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-mouse-2014-10-21.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
 #pd = pd[,c("individual", "treatment", "biorep", "techrep", "label")]
-pd2 = pd2[,c("sample", "biorep", "passage", "lane", "label")]
+pd2 = pd1[,c("sample", "biorep", "passage", "lane", "label")]
 
-pd3 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-2014-11-12.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
+
+pd3 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-mouse-2014-11-10.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
 #pd = pd[,c("individual", "treatment", "biorep", "techrep", "label")]
-pd3 = pd3[,c("sample", "biorep", "passage", "lane", "label")]
+pd3 = pd2[,c("sample", "biorep", "passage", "lane", "label")]
 
-pd =  rbind(pd1, pd2, pd3)
+pd4 = read.delim(file=file.path(folder, "1-Alignments/sampleAnnot-human-2014-11-12.formated.txt"), stringsAsFactors=FALSE, check.names=FALSE)
+#pd = pd[,c("individual", "treatment", "biorep", "techrep", "label")]
+pd4 = pd3[,c("sample", "biorep", "passage", "lane", "label")]
+
+pd =  rbind(pd1, pd2, pd3, pd4)
 
 ## introns 
 gs = genes(hsa)
