bd3b7d482cc78032719739876f1b2a1c1e8cb1c2
hanice <sunhanice@gmail.com>
2015-01-27 01:30:05 +0100

    hanice

--- Stanford/1-Alignments/5-mapping-quality/2-plot.R
+++ Stanford/1-Alignments/5-mapping-quality/2-plot.R
@@ -1,16 +1,17 @@
 library(ggplot2)
-library(reshape2)
+gg_color_hue <- function(n) {
+      hues = seq(15, 375, length=n+1)
+  hcl(h=hues, l=65, c=100)[1:n]
+}
 rt =read.table('5-wrong-map-mouse-to-human2.formated')
 data = rt[,c(1,3)]
-colnames(data)=c("Sample","Percentage")
-melted <- melt(data, id.vars=c("Sample"))
-#qplot(x=ordered(as.factor(Sample),c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), y='Reads Count', data=rt, geom="bar", stat="identity",position="dodge", fill="white",colour="darkgreen")
-#qplot(x=Sample, y=Count, data=rt, geom="bar", stat="identity",position="dodge")
-pdf('NGLY1-Virus-Bacterium-Fibroblast.pdf')
-#c <- ggplot(melted, aes(x=factor(Sample, levels=c('CP1','CP2','CP3','CP4','FCP1','MCP1')),y=value),fill=variable)
-#c + geom_bar(stat="identity", width=0.7) + ylab("The number of reads from human herpesvirus 4 (EBV)") + xlab("Sample of B cells")
-#dev.off()
-qplot(x=factor(Sample), y=value, data=melted, geom="bar", stat="identity",position="dodge",xlab="Sample", ylab="Percentage")
-dev.off()
+data=cbind(rownames(data),data)
+colnames(data) = c('SampleID','Sample','Percentage')
+data = data[order(data$Percentage),]
 
-                                                    
+write.table(data, file ='NGLY1-Mapping-Percentage', quote=F, sep = '\t', row.names =F)
+pdf("NGLY1-Mapping-Percentage.pdf")
+c <- ggplot(data, aes(x=factor(SampleID,level = SampleID),y=Percentage))
+#c + geom_bar(stat="identity", width=0.7, fill=gg_color_hue(2)[1], colour=gg_color_hue(2)[1]) + ylab("Percentage of reads mapped") + xlab("Samples")
+c + geom_bar(stat="identity", width=0.7)  + ylab("Percentage of reads mapped") + xlab("Samples")
+dev.off()
