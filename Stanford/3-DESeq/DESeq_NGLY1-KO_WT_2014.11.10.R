07967d3b1f805591c564787f1506046005609c8b
hanice <sunhanice@gmail.com>
2015-01-28 21:00:03 +0100

    hanice

---
+++ Stanford/3-DESeq/DESeq_NGLY1-KO_WT_2014.11.10.R
@@ -0,0 +1,69 @@
+# test for DE in patient vs control group
+# Author: czhu
+###############################################################################
+library(GenomicRanges)
+library(DESeq2)
+
+folder = "/g/steinmetz/hsun/Stanford"
+
+outfolder = file.path(folder, "3-DESeq")
+if (!file.exists(outfolder))  dir.create(outfolder)
+
+load(file.path(folder, "Counts-Mouse-2014-1011.rda"))
+#load(file.path(folder, "sampleAnnot-CP4.rda"))
+
+
+## first look at gene counts
+mat = assay(geneCounts)
+
+## remove 19 for the moment and only consider the mock treated samples
+#wh = which(sampleAnnot$individual != 19 & sampleAnnot$treatment == "DMSO")
+#for patient specific analysis
+#wh = which(grepl("^CP.*", sampleAnnot$individual) & sampleAnnot$treatment == "DMSO" )
+##interfamily differences
+#wh = which(sampleAnnot$family == "w" & sampleAnnot$treatment == "DMSO")
+#CP2 v CP3
+wh = which(grepl("^CP[14].*", sampleAnnot$individual) & sampleAnnot$treatment == "DMSO" )
+wh = which(grepl('.*(NGLY1-KO|WT).*2014.11.10',colnames(geneCounts)))
+
+sampleAnnot = droplevels(sampleAnnot[wh,])
+mat = mat[, wh]
+
+#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sampleOrigin+ sampleStatus + treatment)
+#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~gender + sampleStatus)
+dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~individual) #to compare CP2 and CP3 to CP1, unable to use family/gender difference, same comparison here
+dds = DESeq(dds)
+
+#dds = DESeq(dds, test="LRT", reduce=~treatment)
+load(file.path(folder, "gtf.rda"))
+res = results(dds)
+res = cbind.data.frame(res, ids[match(rownames(res), ids$gene_id), c("gene_name","gene_biotype")])
+res = res[order(res$padj), ]
+
+#..#write.table( res, file=file.path(outfolder, "deCP1vCP4.txt"), quote = FALSE, sep = "\t",  row.names = FALSE)
+write.table( res, file=file.path(outfolder, "deCP1CP4.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+
+rld = rlog(dds, blind=FALSE)
+save(dds, rld, res,sampleAnnot,file=file.path(outfolder, "resCP1vCP4.rda"))
+
+pdf(file.path(outfolder, "plot_PCA-deCP1CP4.pdf"), width=8, height=6)
+print(plotPCA(rld, intgroup=c("individual", "sampleStatus")))
+dev.off()
+
+pdf(file.path(outfolder, "plot_MA-deCP1CP4.pdf"), width=8, height=6)
+plotMA(results(dds), alpha=0.01)
+dev.off()
+
+pdf(file.path(outfolder, "plot_dispEst-deCP1CP4.pdf"), width=8, height=6)
+plotDispEsts(dds)
+dev.off()
+
+#library("ReportingTools")
+#desReport <- HTMLReport(shortName = 'RNAseq_analysis_with_DESeq',
+#    title = 'RNA-seq analysis of differential expression using DESeq',reportDirectory = "./reports")
+#res$id = df[match(rownames(res),df$ensembl_id),"gene_id"]
+#
+#publish(res,desReport,name="df",countTable=mat, pvalueCutoff=0.01,
+#    conditions=sampleAnnot,annotation.db="org.Hs.eg.db",
+#    expName="deseq",reportDir="./reports", .modifyDF=makeDESeqDF)
+#finish(desReport)
