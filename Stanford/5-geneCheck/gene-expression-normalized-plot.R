5c3381d4ac076f4c210b8ef6fc1dad64018b7e70
hanice <sunhanice@gmail.com>
2015-02-12 21:30:02 +0100

    hanice

--- Stanford/5-geneCheck/gene-expression-normalized-plot.R
+++ Stanford/5-geneCheck/gene-expression-normalized-plot.R
@@ -3,16 +3,20 @@ folder = '/g/steinmetz/hsun/Stanford'
 load(file.path(folder,'Counts-Mouse-2014-1011.rda'))
 load(file.path(folder,'data/MouseGenome/MouseGTF.rda'))
 
-write.table(data.frame(colnames(geneCounts), colnames(geneCounts)),file=file.path(folder, "/5-geneCheck/MouseSampleAnnot.txt"),col.names=F, row.names=F,sep ="\t",quote=F)
+###### for sample annotation file 
+######write.table(data.frame(colnames(geneCounts), colnames(geneCounts)),file=file.path(folder, "/5-geneCheck/MouseSampleAnnot.txt"),col.names=F, row.names=F,sep ="\t",quote=F)
+##### need manual modify
+sampleAnnot = read.table(file.path(folder,"/5-geneCheck/MouseSampleAnnot.txt"), head=T)
+sampleAnnot$sample = relevel(sampleAnnot$sample,"WT_primaryP4_2014.11.10")
 
-write.table(data.frame(colnames(geneCounts)),file=file.path(folder, "/5-geneCheck/MouseSampleAnnot.txt"),col.names=T, row.names=F,sep ="\t")
 
 mat=assay(geneCounts)
-dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~individual)
+dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sample)
 sf = estimateSizeFactors(dds)
 mat.sf = mat/sizeFactors(sf)
 
-pdf('BCells-NPC1-DYNC1H1-Normalized-Expression.pdf')
-lattice::dotplot(mat.sf[ids[ids$gene_name == 'NPC1',]$gene_id,]~factor(sampleAnnot$individual,levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NPC1")
-lattice::dotplot(mat.sf[ids[ids$gene_name == 'DYNC1H1',]$gene_id,]~factor(sampleAnnot$individual,levels=c('CP1-B','CP3-B','MCP1-B','Ctrl-B')), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="DYHC1H1")
+pdf(file.path(folder, '/5-geneCheck/Mouse-NGLY1-Normalized-Expression.pdf'))
+#lattice::dotplot(mat.sf[ids[ids$gene_name == 'NGLY1',]$gene_id,]~factor(sampleAnnot$sample), group=sampleAnnot$sampleStatus, auto.key=TRUE,pch=19, ylab="Gene Expression",main="NPC1")
+lattice::dotplot(mat.sf[ids[ids$gene_name == 'Ngly1',]$gene_id,]~factor(sampleAnnot$sample), auto.key=TRUE,pch=19, ylab="Gene Expression",main="NGLY1")
+lattice::dotchart(mat.sf["ENSMUSG00000021785",]~factor(sampleAnnot$sample), auto.key=TRUE,pch=19, ylab="Gene Expression",main="NGLY1")
 dev.off()
