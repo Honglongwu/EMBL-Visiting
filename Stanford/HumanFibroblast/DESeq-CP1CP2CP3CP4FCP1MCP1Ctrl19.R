901149161da3bedd5f0743f6f5132ea7df3386da
hanice <sunhanice@gmail.com>
2015-02-15 22:00:04 +0100

    hanice

--- Stanford/HumanFibroblast/DESeq-CP1CP2CP3CP4FCP1MCP1Ctrl19.R
+++ Stanford/HumanFibroblast/DESeq-CP1CP2CP3CP4FCP1MCP1Ctrl19.R
@@ -4,13 +4,13 @@
 library(GenomicRanges)
 library(DESeq2)
 
-folder = "/g/steinmetz/wmueller/NGLY1"
-
-outfolder = file.path(folder, "NGLY1-groupwise-GeneLevel")
+folder = "/g/steinmetz/hsun/Stanford/HumanFibroblast"
+outfolder = folder
+#outfolder = file.path(folder, "NGLY1-groupwise-GeneLevel")
 if (!file.exists(outfolder))  dir.create(outfolder)
 
-load(file.path(folder, "counts-CP4.rda"))
-load(file.path(folder, "sampleAnnot-CP4.rda"))
+load(file.path(folder, "Counts-CP1CP2CP3CP4FCP1MCP1Ctrl19.rda"))
+load(file.path(folder, "SampleAnnot-CP1CP2CP3CP4FCP1MCP1Ctrl19.rda"))
 
 ## first look at gene counts
 mat = assay(geneCounts)
@@ -22,7 +22,8 @@ mat = assay(geneCounts)
 ##interfamily differences
 #wh = which(sampleAnnot$family == "w" & sampleAnnot$treatment == "DMSO")
 #CP2 v CP3
-wh = which(grepl("FCP1|^CP[23].*", sampleAnnot$individual) & sampleAnnot$treatment == "DMSO" )
+#wh = which(sampleAnnot$treatment == "DMSO")
+wh = wh = c(7,8,11,12,15,16,19,20,23,24,27,28,3,4)
 
 sampleAnnot = droplevels(sampleAnnot[wh,])
 sampleAnnot$individual=relevel(sampleAnnot$individual,'FCP1')
@@ -30,41 +31,36 @@ mat = mat[, wh]
 
 #dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sampleOrigin+ sampleStatus + treatment)
 #dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~gender + sampleStatus)
-dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~individual) #to compare CP2 and CP3 to CP1, unable to use family/gender difference, same comparison here
+#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~individual) #to compare CP2 and CP3 to CP1, unable to use family/gender difference, same comparison here
+dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sampleStatus+gender) #to compare CP2 and CP3 to CP1, unable to use family/gender difference, same comparison here
 dds = DESeq(dds)
 
 #dds = DESeq(dds, test="LRT", reduce=~treatment)
-load(file.path(folder, "gtf.rda"))
+load(file.path(folder, "/g/steinmetz/hsun/Stanford/data/HumanGTF.rda"))
 res = results(dds)
 res = cbind.data.frame(res, ids[match(rownames(res), ids$gene_id), c("gene_name","gene_biotype")])
 res = res[order(res$padj), ]
-res.sig = res[which(res$padj<0.01),]
+res.sig = res[which(res$padj<0.05),]
+res.sig.proteincoding = res.sig[res.sig$gene_biotype == "protein_coding",]
+res.sig.nonproteincoding = res.sig[res.sig$gene_biotype != "protein_coding",]
 
 #..#write.table( res, file=file.path(outfolder, "deCP1vCP4.txt"), quote = FALSE, sep = "\t",  row.names = FALSE)
-write.table( res, file=file.path(outfolder, "deCP2CP3FCP1.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
-write.table( res.sig, file=file.path(outfolder, "deCP2CP3FCP1_sig.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res, file=file.path(outfolder, "deCP1CP2CP3CP4FCP1MCP1Ctrl19.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res.sig, file=file.path(outfolder, "deCP1CP2CP3CP4FCP1MCP1Ctrl19_sig.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res.sig.proteincoding, file=file.path(outfolder, "deCP1CP2CP3CP4FCP1MCP1Ctrl19_sig_proteincoding.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
+write.table( res.sig.nonproteincoding, file=file.path(outfolder, "deCP1CP2CP3CP4FCP1MCP1Ctrl19_sig_nonproteincoding.txt"), quote = FALSE, sep = "\t",  row.names = T, col.names=NA)
 
 rld = rlog(dds, blind=FALSE)
-save(dds, rld, res, res.sig,sampleAnnot,file=file.path(outfolder, "resCP2CP3FCP1.rda"))
+save(dds, rld, res, res.sig,sampleAnnot,file=file.path(outfolder, "resCP1CP2CP3CP4FCP1MCP1Ctrl19.rda"))
 
-pdf(file.path(outfolder, "plot_PCA-deCP2CP3FCP1.pdf"), width=8, height=6)
+pdf(file.path(outfolder, "plot_PCA-deCP1CP2CP3CP4FCP1MCP1Ctrl19.pdf"), width=8, height=6)
 print(plotPCA(rld, intgroup=c("individual", "sampleStatus")))
 dev.off()
 
-pdf(file.path(outfolder, "plot_MA-deCP2CP3FCP1.pdf"), width=8, height=6)
+pdf(file.path(outfolder, "plot_MA-deCP1CP2CP3CP4FCP1MCP1Ctrl19.pdf"), width=8, height=6)
 plotMA(results(dds), alpha=0.01)
 dev.off()
 
-pdf(file.path(outfolder, "plot_dispEst-deCP2CP3FCP1.pdf"), width=8, height=6)
+pdf(file.path(outfolder, "plot_dispEst-deCP1CP2CP3CP4FCP1MCP1Ctrl19.pdf"), width=8, height=6)
 plotDispEsts(dds)
 dev.off()
-
-#library("ReportingTools")
-#desReport <- HTMLReport(shortName = 'RNAseq_analysis_with_DESeq',
-#    title = 'RNA-seq analysis of differential expression using DESeq',reportDirectory = "./reports")
-#res$id = df[match(rownames(res),df$ensembl_id),"gene_id"]
-#
-#publish(res,desReport,name="df",countTable=mat, pvalueCutoff=0.01,
-#    conditions=sampleAnnot,annotation.db="org.Hs.eg.db",
-#    expName="deseq",reportDir="./reports", .modifyDF=makeDESeqDF)
-#finish(desReport)
