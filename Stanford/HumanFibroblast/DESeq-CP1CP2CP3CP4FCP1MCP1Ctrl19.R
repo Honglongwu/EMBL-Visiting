1a585640904e364658e5e78aa1d11e9259a91f57
hanice <sunhanice@gmail.com>
2015-02-16 00:00:02 +0100

    hanice

--- Stanford/HumanFibroblast/DESeq-CP1CP2CP3CP4FCP1MCP1Ctrl19.R
+++ Stanford/HumanFibroblast/DESeq-CP1CP2CP3CP4FCP1MCP1Ctrl19.R
@@ -27,6 +27,7 @@ wh = c(7,8,11,12,15,16,19,20,23,24,27,28,3,4)
 
 sampleAnnot = droplevels(sampleAnnot[wh,])
 sampleAnnot$individual=relevel(sampleAnnot$individual,'FCP1')
+rownames(sampleAnnot) = sampleAnnot$label
 mat = mat[, wh]
 
 #dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sampleOrigin+ sampleStatus + treatment)
---
+++ Stanford/HumanFibroblastLymphoblast/1-intersect.py
@@ -0,0 +1,39 @@
+def intersect(inF1, inF2, ouF):
+    D = {}
+    L = []
+    inFile = open(inF1)
+    head = inFile.readline().strip('\n')
+    for line in inFile:
+        line = line.strip('\n')
+        fields = line.split('\t')
+        gene = fields[0]
+        D.setdefault(gene, [])
+        D[gene].append(line)
+        L.append(gene)
+    inFile.close()
+
+    inFile = open(inF2)
+    head = inFile.readline().strip('\n')
+    for line in inFile:
+        line = line.strip('\n')
+        fields = line.split('\t')
+        gene = fields[0]
+        D.setdefault(gene, [])
+        D[gene].append(line)
+    inFile.close()
+
+    ouFile = open(ouF, 'w')
+    for k in L:
+        if len(D[k]) == 2:
+            ouFile.write('\t'.join(D[k]) + '\n')
+    ouFile.close()
+
+inF1 = 'deCP1CP2CP3CP4FCP1MCP1Ctrl19_sig_proteincoding_up.txt'
+inF2 = 'deCP1CP3MCP1Ctrl_sig_proteincoding_up.txt'
+ouF = 'Human-Fibroblast-Lymphoblast-sig-proteincoding-up.txt'
+intersect(inF1, inF2, ouF)
+
+inF1 = 'deCP1CP2CP3CP4FCP1MCP1Ctrl19_sig_proteincoding_down.txt'
+inF2 = 'deCP1CP3MCP1Ctrl_sig_proteincoding_down.txt'
+ouF = 'Human-Fibroblast-Lymphoblast-sig-proteincoding-down.txt'
+intersect(inF1, inF2, ouF)
