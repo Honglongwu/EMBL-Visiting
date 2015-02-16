e0995194f3905ad6d01b9d873a5bc199617c15de
hanice <sunhanice@gmail.com>
2015-02-17 00:00:03 +0100

    hanice

--- Stanford/5-geneCheck/03-Human-Lymphoblast-gene-expression-normalized-plot.R
+++ Stanford/5-geneCheck/03-Human-Lymphoblast-gene-expression-normalized-plot.R
@@ -1,15 +1,14 @@
 library(DESeq2)
 folder = '/g/steinmetz/hsun/Stanford'
-load(file.path(folder,'HumanFibroblast/Counts-CP1CP2CP3CP4FCP1MCP1Ctrl19.rda'))
-load(file.path(folder,'HumanFibroblast/SampleAnnot-CP1CP2CP3CP4FCP1MCP1Ctrl19.rda'))
+load(file.path(folder,'HumanLymphoblastNoCtrl/Counts-CP1CP3MCP1Ctrl.rda'))
+load(file.path(folder,'HumanLymphoblastNoCtrl/SampleAnnot-CP1CP3CP1Ctrl.rda'))
 
 load(file.path(folder,'data/HumanGTF.rda'))
 
 
-wh = c(7,8,11,12,15,16,19,20,23,24,27,28)
+wh=c(1,2,3,4,7,8)
 
 sampleAnnot = droplevels(sampleAnnot[wh,])
-sampleAnnot$individual=relevel(sampleAnnot$individual,'FCP1')
 rownames(sampleAnnot) = sampleAnnot$label
 
 mat=assay(geneCounts)
@@ -28,7 +27,7 @@ ddsed.norm = counts(ddsed,norm=T)
 
 gene.plot=function(gene)
 {
-pdf(file.path(folder, paste0('/5-geneCheck/Human-Fibroblast-',gene,'-Normalized-Expression.pdf')))
+pdf(file.path(folder, paste0('/5-geneCheck/Human-Lymphoblast-',gene,'-Normalized-Expression.pdf')))
 gn = unname(unlist(ddsed.norm[ids[ids$gene_name==gene,1,],]))
 data = data.frame(sampleAnnot,gn)
 
@@ -39,7 +38,6 @@ dev.off()
 
 
 gene.plot("UCHL1")
-gene.plot("DYNC1H1")
 
 
 #gene = unname(unlist(ddsed.norm["ENSG00000154277",]))
