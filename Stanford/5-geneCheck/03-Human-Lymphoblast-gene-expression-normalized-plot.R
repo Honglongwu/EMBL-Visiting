9d29f90687e2e4de7b39755b956608fea0abd677
hanice <sunhanice@gmail.com>
2015-02-17 00:30:02 +0100

    hanice

--- Stanford/5-geneCheck/03-Human-Lymphoblast-gene-expression-normalized-plot.R
+++ Stanford/5-geneCheck/03-Human-Lymphoblast-gene-expression-normalized-plot.R
@@ -1,7 +1,7 @@
 library(DESeq2)
 folder = '/g/steinmetz/hsun/Stanford'
 load(file.path(folder,'HumanLymphoblastNoCtrl/Counts-CP1CP3MCP1Ctrl.rda'))
-load(file.path(folder,'HumanLymphoblastNoCtrl/SampleAnnot-CP1CP3CP1Ctrl.rda'))
+load(file.path(folder,'HumanLymphoblastNoCtrl/SampleAnnot-CP1CP3MCP1Ctrl.rda'))
 
 load(file.path(folder,'data/HumanGTF.rda'))
 
@@ -31,7 +31,7 @@ pdf(file.path(folder, paste0('/5-geneCheck/Human-Lymphoblast-',gene,'-Normalized
 gn = unname(unlist(ddsed.norm[ids[ids$gene_name==gene,1,],]))
 data = data.frame(sampleAnnot,gn)
 
-lattice::dotplot(gn~individual,group=sampleStatus,data=data, auto.key=T, pch=19, ylab="Normalized Gene Expression",xlab=gene) 
+print(lattice::dotplot(gn~individual,group=sampleStatus,data=data, auto.key=T, pch=19, ylab="Normalized Gene Expression",xlab=paste0(gene," in lymphoblast cells")))
 dev.off()
 }
 
