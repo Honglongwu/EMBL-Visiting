5f61fff24635e532125382339e5459e28db01259
hanice <sunhanice@gmail.com>
2015-02-19 01:30:03 +0100

    hanice

---
+++ Stanford/HumanFibroblastNoCtrl19/01-proteincoding-expressed.py
@@ -0,0 +1,11 @@
+inFile = open('deCP1CP2CP3CP4FCP1MCP1.txt')
+ouFile = open('Fibroblast-CP1CP2CP3CP4FCP1MCP1-proteincoding-expressed-genes.txt', 'w')
+head = inFile.readline()
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    if fields[-1] == 'protein_coding':
+        if float(fields[1]) > 20:
+            ouFile.write(fields[0] + '\t' + fields[-2]+ '\n')
+inFile.close()
+ouFile.close()
---
+++ Stanford/HumanLymphoblastWithCtrl/01-proteincoding-expressed.py
@@ -0,0 +1,11 @@
+inFile = open('deCP1CP3MCP1Ctrl.txt')
+ouFile = open('Lymphoblast-CP1CP3MCP1Ctrl-proteincoding-expressed-genes.txt', 'w')
+head = inFile.readline()
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    if fields[-1] == 'protein_coding':
+        if float(fields[1]) > 20:
+            ouFile.write(fields[0] + '\t' + fields[-2]+ '\n')
+inFile.close()
+ouFile.close()
