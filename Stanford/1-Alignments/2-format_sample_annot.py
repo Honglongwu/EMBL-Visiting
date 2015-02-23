3eede1567ac503ce52a97fe3ef4e60aa5e107a28
hanice <sunhanice@gmail.com>
2015-02-23 21:00:02 +0100

    hanice

--- Stanford/1-Alignments/2-format_sample_annot.py
+++ Stanford/1-Alignments/2-format_sample_annot.py
@@ -207,13 +207,86 @@ def format(F):
             laneName = fields[3]
             name = fields[4]
             fds = name.split('_')
-            if fds[2] in ['1','2','3','4','5']:
-                sample = fds[1]
-                biorep = 'biorep' + fds[2]
-            else:
+            if fds[2] == 'B':
                 sample = fds[1] + '-' + fds[2]
                 biorep = 'biorep' + fds[3]
+            elif fds[2] == 'iPS':
+                sample = fds[1] + '-' + fds[2]
+                biorep = 'biorep2'
+            else:
+                sample = fds[1]
+                biorep = 'biorep' + fds[2]
             passage = 'None'
+            lane = fds[-1]
+            #print('\t'.join([sample, biorep, passage, lane]))
+            if passage != 'None':
+                label = sample + '_' + biorep + '_' + passage
+            else:
+                label = sample + '_' + biorep
+            L.append([sampleName, barcode, filename, laneName, name, sample, biorep, passage, lane, label])
+                
+            #print('\t'.join([sampleName, barcode, lane, name]))
+        L.sort(key=itemgetter(5,7,6,8))
+        
+        ouFile.write('\t'.join(['SampleName', 'Barcode', 'Filename', 'LaneName', 'name', 'sample', 'biorep', 'passage', 'lane', 'label']) + '\n')
+    
+        for item in L:
+            ouFile.write('\t'.join(item) + '\n')
+            print(item[-1])
+
+    elif F.find('human-2015-01-07.txt') != -1:
+        head = inFile.readline()
+        L = []
+        for line in inFile:
+            line = line.strip()
+            fields = line.split('\t')
+            sampleName = fields[0]
+            barcode = fields[1]
+            filename = fields[2]
+            laneName = fields[3]
+            name = fields[4]
+            fds = name.split('_')
+            sample = fds[1]
+            biorep = 'biorep' + fds[2]
+            passage = 'None'
+            lane = fds[-1]
+            #print('\t'.join([sample, biorep, passage, lane]))
+            if passage != 'None':
+                label = sample + '_' + biorep + '_' + passage
+            else:
+                label = sample + '_' + biorep
+            L.append([sampleName, barcode, filename, laneName, name, sample, biorep, passage, lane, label])
+                
+            #print('\t'.join([sampleName, barcode, lane, name]))
+        L.sort(key=itemgetter(5,7,6,8))
+        
+        ouFile.write('\t'.join(['SampleName', 'Barcode', 'Filename', 'LaneName', 'name', 'sample', 'biorep', 'passage', 'lane', 'label']) + '\n')
+    
+        for item in L:
+            ouFile.write('\t'.join(item) + '\n')
+            print(item[-1])
+
+    elif F.find('mouse-2015-01-07.txt') != -1:
+        head = inFile.readline()
+        L = []
+        for line in inFile:
+            line = line.strip()
+            fields = line.split('\t')
+            sampleName = fields[0]
+            barcode = fields[1]
+            filename = fields[2]
+            laneName = fields[3]
+            name = fields[4]
+            fds = name.split('_')
+            sample = fds[1] + '-' + fds[2]
+            biorep = 'biorep' + fds[3]
+            print(sample)
+            if sample == 'NGLY1-KO':
+                passage = 'immortP3'
+            elif sample == 'Double-KO':
+                passage = 'primaryP4'
+            elif sample == 'ENGase-KO':
+                passage = 'primaryP4'
             lane = fds[-1]
             #print('\t'.join([sample, biorep, passage, lane]))
             if passage != 'None':
@@ -233,6 +306,7 @@ def format(F):
 
 
 
+
     inFile.close()
     ouFile.close()
 
@@ -244,3 +318,5 @@ def format(F):
 #format('sampleAnnot-human-2014-11-12.txt')
 
 format('sampleAnnot-human-2014-11-24.txt')
+format('sampleAnnot-human-2015-01-07.txt')
+format('sampleAnnot-mouse-2014-11-24.txt')
