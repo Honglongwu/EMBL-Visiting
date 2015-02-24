b36cb71330ad0332d21c2a0767373bf1e3c57b80
hanice <sunhanice@gmail.com>
2015-02-24 20:30:02 +0100

    hanice

--- Stanford/1-Alignments/Fastqc/fastqc.sh
+++ Stanford/1-Alignments/Fastqc/fastqc.sh
@@ -1,2 +1,4 @@
-fastqc -o . ngly1_CP1_iPS_LIB18452_RBA17677_1.txt.gz
-fastqc -o . ngly1_CP1_iPS_LIB18460_RBA17685_1.txt.gz
+#fastqc -o . ngly1_CP1_iPS_LIB18452_RBA17677_1.txt.gz
+#fastqc -o . ngly1_CP1_iPS_LIB18460_RBA17685_1.txt.gz
+#fastqc -o . ngly1_CP1-iPS_1_LIB18622_RBA17847_1.txt.gz
+fastqc -o . ngly1_MCP1-iPS_2_LIB18666_RBA17891_1.txt.gz
