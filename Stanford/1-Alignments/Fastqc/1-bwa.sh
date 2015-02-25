dbaf50d31876ed5251152251eda76865eaf82964
hanice <sunhanice@gmail.com>
2015-02-25 22:30:02 +0100

    hanice

--- Stanford/1-Alignments/Fastqc/1-bwa.sh
+++ Stanford/1-Alignments/Fastqc/1-bwa.sh
@@ -1,1 +1,2 @@
-bwa aln -t 8 
+bwa aln -t 8 /g/steinmetz/hsun/Stanford/data/bwa/HumanGenome  ngly1_CP1_iPS_LIB18452_RBA17677_1.txt.gz >ngly1_CP1_iPS_LIB18452_RBA17677_1.sai
+bwa samse /g/steinmetz/hsun/Stanford/data/bwa/HumanGenome ngly1_CP1_iPS_LIB18452_RBA17677_1.sai ngly1_CP1_iPS_LIB18452_RBA17677_1.txt.gz >ngly1_CP1_iPS_LIB18452_RBA17677_1.sam
