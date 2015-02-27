8bd2842ae6cb29c9cf721a17f8dde484796ecf44
hanice <sunhanice@gmail.com>
2015-02-27 23:30:03 +0100

    hanice

--- Stanford/HumanALL/gene-plot.R
+++ Stanford/HumanALL/gene-plot.R
@@ -1,34 +1,15 @@
 library(DESeq2)
-folder = '/g/steinmetz/hsun/Stanford'
-load(file.path(folder,'HumanLymphoblastWithCtrl/Counts-Human-Lymphoblast.rda'))
-load(file.path(folder,'HumanLymphoblastWithCtrl/sampleAnnot.rda'))
-load(file.path(folder,'data/HumanGTF.rda'))
 
-
-sampleAnnot = droplevels(sampleAnnot[c(1,2,3,4,5,6,7,8,9,10),])
-rownames(sampleAnnot) = sampleAnnot$label
-
-mat=assay(geneCounts)
-mat = mat[, c(1,2,3,4,7,8,9,10,5,6)]
-
-#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sampleOrigin+ sampleStatus + treatment)
-#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~gender + sampleStatus)
-#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~individual) #to compare CP2 and CP3 to CP1, unable to use family/gender difference, same comparison here
-dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~gender+sampleStatus) #to compare CP2 and CP3 to CP1, unable to use family/gender difference, same comparison here
-ddsed = DESeq(dds)
-ddsed.norm = counts(ddsed,norm=T)
-
-#dds = DESeqDataSetFromMatrix(mat, sampleAnnot, design=~sample)
-#sf = estimateSizeFactors(dds)
-#mat.sf = mat/sizeFactors(sf)
-
+load('resNGLY1.rda')
+load('/g/steinmetz/hsun/Stanford/data/HumanGTF.rda')
+ddsed.norm = counts(dds,norm=T)
 gene.plot=function(gene)
 {
-pdf(file.path(folder, paste0('/5-geneCheckUpdated/Human-Lymphoblast-Top10-',gene,'-Normalized-Expression.pdf')))
+pdf(paste0(gene,'-Normalized-Expression.pdf'))
 gn = unname(unlist(ddsed.norm[ids[ids$gene_name==gene,1,],]))
 data = data.frame(sampleAnnot,gn)
 
-print(lattice::dotplot(gn~factor(sample,level=c("Ctrl-B", "FCP1-B","MCP1-B","CP1-B","CP3-B")),group=factor(sampleStatus),data=data, auto.key=T, pch=19, ylab="Normalized gene expression",xlab=paste0(gene," in lymphoblast cells")))
+print(lattice::dotplot(gn~individual|cellType,group=sampleStatus,data=data, auto.key=T, pch=19, ylab="Normalized gene expression",xlab=paste0(gene," in lymphoblast cells")))
 dev.off()
 }
 
@@ -54,16 +35,12 @@ dev.off()
 #gene.plot("RBPJ")
 
 
-gene.plot("UCHL1")
-gene.plot("DHRS9")
-gene.plot("TNFRSF19")
-gene.plot("SPARC")
-gene.plot("TJP1")
-gene.plot("EPHB1")
-gene.plot("CNR1")
-gene.plot("TNS3")
-gene.plot("TBX15")
-gene.plot("RAB38")
+gene.plot("NGLY1")
+gene.plot("PSMB1")
+gene.plot("PSMD1")
+gene.plot("PSMD11")
+gene.plot("PSMD14")
+gene.plot("PSMC2")
 
 
 #gene = unname(unlist(ddsed.norm["ENSG00000154277",]))
