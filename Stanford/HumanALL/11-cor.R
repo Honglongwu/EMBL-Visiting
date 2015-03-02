b8c1fe0a0ad341a1c67636d35e91edb7fc75caf5
hanice <sunhanice@gmail.com>
2015-03-02 22:30:04 +0100

    hanice

--- Stanford/HumanALL/11-cor.R
+++ Stanford/HumanALL/11-cor.R
@@ -46,4 +46,15 @@ for (i in seq(1,dim(mat)[1]))
     c = cor(x, y)
     out = rbind(out, c(ids[ids$gene_id==gene,'gene_name'], ids[ids$gene_id==rownames(mat)[i],'gene_name'], gene, rownames(mat)[i], c))
 }
-write.table(out, file = 'NGLY1-Genes-Correlation.txt', sep = '\t', quote=F, row.names=F, col.names=F)
+write.table(out, file = 'NGLY1-Genes-Correlation-iPS.txt', sep = '\t', quote=F, row.names=F, col.names=F)
+
+out = c('FirstGeneSymbol', 'SecondGeneSymbol', 'FirstGeneID', 'SecondGeneID', 'cor')
+for (i in seq(1,dim(mat)[1]))
+{
+    gene = 'ENSG00000151092'
+    x = mat[i,][31,32]
+    y = mat[gene,]
+    c = cor(x, y)
+    out = rbind(out, c(ids[ids$gene_id==gene,'gene_name'], ids[ids$gene_id==rownames(mat)[i],'gene_name'], gene, rownames(mat)[i], c))
+}
+write.table(out, file = 'NGLY1-Genes-Correlation-NPC.txt', sep = '\t', quote=F, row.names=F, col.names=F)
