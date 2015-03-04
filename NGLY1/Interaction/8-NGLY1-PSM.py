d7c176d2466f313044682b334a3932f2e99fc1ee
hanice <sunhanice@gmail.com>
2015-03-04 01:00:04 +0100

    hanice

--- NGLY1/Interaction/8-NGLY1-PSM.py
+++ NGLY1/Interaction/8-NGLY1-PSM.py
@@ -1,7 +1,16 @@
 inFile = open('BIOGRID-ALL-Interaction')
+D = {}
+ouFile1 = open('BIOGRID-ALL-Interaction-PSM-1', 'w')
+ouFile2 = open('BIOGRID-ALL-Interaction-PSM-2', 'w')
 for line in  inFile:
     line = line.strip()
     fields = line.split('\t')
     if fields[0][0:3] == 'PSM'  and fields[1][0:3] == 'PSM':
-        print(line)
+        ouFile1.write(line + '\n')
+        D.setdefault(fields[0], 0)
+        D[fields[0]] += 1
+        D.setdefault(fields[1], 0)
+        D[fields[1]] += 1
 inFile.close()
+for k in D:
+    ouFile2.write(k + '\t' + str(D[k]) + '\n')
