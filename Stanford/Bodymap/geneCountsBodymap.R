9c3ea50809ee758f2dd48b1c7ee8d44a9b5919f7
hanice <sunhanice@gmail.com>
2015-03-11 18:30:04 +0100

    hanice

---
+++ Stanford/Bodymap/geneCountsBodymap.R
@@ -0,0 +1,7 @@
+load('Counts-Human-Bodymap.rda')
+load('/g/steinmetz/hsun/Stanford/data/HumanGTF.rda')
+library(DESeq2)
+mat = assay(geneCounts)
+gene.name = ids[match(rownames(mat),ids$gene_id),'gene_name']
+mat.name = cbind(gene.name,mat)
+write.table(mat.name,file='geneCountsBodymap.txt',row.names=T,col.names=NA,sep='\t',quote=F)
