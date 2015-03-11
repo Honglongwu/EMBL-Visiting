fd055293a05e9ae003ecec0c3a41f3e85f2acc57
hanice <sunhanice@gmail.com>
2015-03-11 21:30:03 +0100

    hanice

--- Stanford/Bodymap/geneCountsBodymap-Stat.py
+++ Stanford/Bodymap/geneCountsBodymap-Stat.py
@@ -1,2 +1,11 @@
-inFile = open()
+inFile = open('geneCountsBodymap.txt')
+head = inFile.readline('\n').strip('\t')
+for line in inFile:
+    line = line.strip()
+    fields = line.split('\t')
+    for i in range(3, len(fields)):
+        D.setdefault(head[i], [])
+        if int(fields[i]) > 30:
+            D[head[i]].append(fields[i])
+
 inFile.close()
--- Stanford/Bodymap/geneCountsBodymap.R
+++ Stanford/Bodymap/geneCountsBodymap.R
@@ -2,6 +2,6 @@ load('Counts-Human-Bodymap.rda')
 load('/g/steinmetz/hsun/Stanford/data/HumanGTF.rda')
 library(DESeq2)
 mat = assay(geneCounts)
-gene.name = ids[match(rownames(mat),ids$gene_id),'gene_name']
+gene.name = ids[match(rownames(mat),ids$gene_id),c('gene_name','gene_biotype')]
 mat.name = cbind(gene.name,mat)
 write.table(mat.name,file='geneCountsBodymap.txt',row.names=T,col.names=NA,sep='\t',quote=F)
