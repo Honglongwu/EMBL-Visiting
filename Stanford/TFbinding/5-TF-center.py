22c9606d05673a9a15208694aa3d735b5af071a3
hanice <sunhanice@gmail.com>
2015-03-12 07:00:03 +0100

    hanice

--- Stanford/TFbinding/5-TF-center.py
+++ Stanford/TFbinding/5-TF-center.py
@@ -1,8 +1,16 @@
+D = {}
 inFile = open('Genes-Interested-TF')
 for line in inFile:
     line = line.strip()
     fields = line.split('\t')
-    tf = fields[]
-    gene = fields[]
-
+    tf = fields[8]
+    gene = fields[0]
+    D.setdefault(tf, [])
+    D[tf].append(gene)
 inFile.close()
+d = D.items()
+d.sort(cmp = lambda x,y :cmp(len(set(x[1])), len(set(y[1]))),reverse=True)
+ouFile = open('Genes-Interested-TF-sorted','w')
+for item in d:
+    ouFile.write(item[0] +'\t'+ '\t'.join(set(item[1])) + '\n')
+ouFile.close()
