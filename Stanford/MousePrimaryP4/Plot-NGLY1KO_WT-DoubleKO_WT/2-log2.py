a6257aa29aadc3477b2d8ed05e794c6ed5fba609
hanice <sunhanice@gmail.com>
2015-03-20 07:30:03 +0100

    hanice

--- Stanford/MousePrimaryP4/Plot-NGLY1KO_WT-DoubleKO_WT/2-log2.py
+++ Stanford/MousePrimaryP4/Plot-NGLY1KO_WT-DoubleKO_WT/2-log2.py
@@ -1,5 +1,5 @@
-inFile = open('de_NGLY1-KO_WT.txt')
 D = {}
+inFile = open('de_NGLY1-KO_WT.txt')
 head = inFile.readline()
 for line in inFile:
     fields = line.split('\t')
@@ -15,7 +15,33 @@ for line in inFile:
     D[fields[0]].append(fields[2])
 inFile.close()
 
-ouFile = open('NGLY1-KO_WT_Double-KO_WT_foldchange','w')
+ouFile = open('Foldchange-NGLY1_KOvsWT-Double_KOvsWT','w')
+ouFile.write('EnsemblID\tNGLY1_KOvsWT\tDouble_KOvsWT\n')
+for k in D:
+    if 'NA' not in D[k]:
+        ouFile.write(k + '\t' + '\t'.join(D[k]) + '\n')
+ouFile.close()
+
+
+D = {}
+inFile = open('de_NGLY1-KO_WT.txt')
+head = inFile.readline()
+for line in inFile:
+    fields = line.split('\t')
+    D.setdefault(fields[0], [])
+    D[fields[0]].append(fields[2])
+inFile.close()
+
+inFile = open('de_NGLY1-KO_Double-KO_2014.10.21.txt')
+head = inFile.readline()
+for line in inFile:
+    fields = line.split('\t')
+    D.setdefault(fields[0], [])
+    D[fields[0]].append(fields[2])
+inFile.close()
+
+ouFile = open('Foldchange-NGLY1_KOvsWT-NGLY1_KOvsDouble_KO','w')
+ouFile.write('EnsemblID\tNGLY1_KOvsWT\tNGLY1_KOvsDouble_KO\n')
 for k in D:
     if 'NA' not in D[k]:
         ouFile.write(k + '\t' + '\t'.join(D[k]) + '\n')
--- Stanford/MousePrimaryP4/Plot-NGLY1KO_WT-DoubleKO_WT/3-scatter.R
+++ Stanford/MousePrimaryP4/Plot-NGLY1KO_WT-DoubleKO_WT/3-scatter.R
@@ -1,3 +1,13 @@
-df = read.table('NGLY1-KO_WT_Double-KO_WT_foldchange')
-plot(df$V2, df$V3)
-abline(lm(df$V2~df$V3), col="red")
+df = read.table('Foldchange-NGLY1_KOvsWT-Double_KOvsWT',head=T)
+pdf('Foldchange-NGLY1_KOvsWT-Double_KOvsWT.pdf')
+plot(df$NGLY1_KOvsWT, df$Double_KOvsWT)
+abline(lm(df$NGLY1_KOvsWT~df$Double_KOvsWT), col="red")
+dev.off()
+
+
+
+df = read.table('Foldchange-NGLY1_KOvsWT-NGLY1_KOvsDouble_KO',head=T)
+pdf('Foldchange-NGLY1_KOvsWT-NGLY1_KOvsDouble_KO.pdf')
+plot(df$NGLY1_KOvsWT, df$NGLY1_KOvsDouble_KO)
+abline(lm(df$NGLY1_KOvsWT~df$NGLY1_KOvsDouble_KO), col="red")
+dev.off()
