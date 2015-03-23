98d58d1eed44178fb32a1ca38cada10dd360b031
hanice <sunhanice@gmail.com>
2015-03-23 01:30:04 +0100

    hanice

---
+++ TIFproteome/XTandem/ch_29May2014_ncRNA_Tryp_HpH_1.sh
@@ -0,0 +1,3 @@
+cd /g/steinmetz/hsun/TIFproteome/XTandem
+
+tandem.exe input-xml/ch_29May2014_ncRNA_Tryp_HpH_1.xml
---
+++ TIFproteome/XTandem/ch_29May2014_ncRNA_Tryp_HpH_6.sh
@@ -0,0 +1,3 @@
+cd /g/steinmetz/hsun/TIFproteome/XTandem
+
+tandem.exe input-xml/ch_29May2014_ncRNA_Tryp_HpH_6.xml
