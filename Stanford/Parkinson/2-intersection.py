5ab16601db9bcc752c4339dca25a8ac9be584c60
hanice <sunhanice@gmail.com>
2015-03-25 00:00:02 +0100

    hanice

--- Stanford/Parkinson/2-intersection.py
+++ Stanford/Parkinson/2-intersection.py
@@ -1,4 +1,5 @@
 def intersection(inF1, inF2):
+    print('***')
     D = {}
     inFile = open(inF1)
     for line in inFile:
@@ -11,10 +12,40 @@ def intersection(inF1, inF2):
         line = line.strip()
         fields = line.split('\t')
         gene = fields[-2]
-        print(gene)
         if gene in D:
             print(gene)
     inFile.close()
 
 intersection('parkinson-down-filtered', 'deNGLY1_sig_proteincoding_down.txt')
 intersection('parkinson-up-filtered', 'deNGLY1_sig_proteincoding_up.txt')
+intersection('parkinson-down-filtered', 'deFibroblast_sig_proteincoding_down.txt')
+intersection('parkinson-up-filtered', 'deFibroblast_sig_proteincoding_up.txt')
+
+
+intersection('parkinson-down-filtered', 'deLymphoblast_sig_proteincoding_down.txt')
+intersection('parkinson-up-filtered', 'deLymphoblast_sig_proteincoding_up.txt')
+
+intersection('parkinson-down-filtered', 'deIPS_sig_proteincoding_down.txt')
+intersection('parkinson-up-filtered', 'deIPS_sig_proteincoding_up.txt')
+
+
+def intersection2(inF1, inF2):
+    print('###')
+    D = {}
+    inFile = open(inF1)
+    for line in inFile:
+        line = line.strip()
+        D[line] = 1
+    inFile.close()
+    
+    inFile = open(inF2)
+    for line in inFile:
+        line = line.strip()
+        fields = line.split('\t')
+        gene = fields[7].upper()
+        if gene in D:
+            print(gene)
+    inFile.close()
+
+intersection2('parkinson-down-filtered', 'de_NGLY1-KO_WT.sig.ortholog.proteincoding.human.down.txt')
+intersection2('parkinson-up-filtered', 'de_NGLY1-KO_WT.sig.ortholog.proteincoding.human.up.txt')
